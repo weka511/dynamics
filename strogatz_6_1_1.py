da09d083b2e6d3a8cf6bdbb3657b877af40d0461
Simon Crase <simon@greenweaves.nz>
2017-12-28 15:37:23 +1300

    Remove duplicate code

--- strogatz_6_1_1.py
+++ strogatz_6_1_1.py
@@ -1,4 +1,4 @@
-import numpy as np, matplotlib.pyplot as plt,math
+import numpy as np, matplotlib.pyplot as plt
 
 from scipy.integrate import odeint
 from matplotlib import rc
@@ -7,21 +7,28 @@ from matplotlib import rc
 rc('font',**{'family':'serif','serif':['Palatino']})
 rc('text', usetex=True)
 
-def f(x,t):
-    return [x[0]+math.exp(-x[1]),-x[1]]
+def ex(x,y):
+    return x+np.exp(-y),-y
 
+def ff(x,t,f=ex):
+    u,v=f(x[0],x[1])
+    return [u]+[v]
+
+nx, ny = 64, 64
+x = np.linspace(-5, 5, nx)
+y = np.linspace(-3, 3, ny)
+X, Y = np.meshgrid(x, y)
+U,V=ex(X,Y)
+
+plt.streamplot(X, Y, U, V, color=U, linewidth=1, cmap=plt.cm.autumn)
 t = np.linspace(0, 10, 101)
 cs = ['r','b','g','m','c','y']
 i=0
-Y, X = np.mgrid[-3:3, -3:3]
-U=X+np.exp(-Y)
-V=-Y
-plt.streamplot(X, Y, U, V, color=U, linewidth=2, cmap=plt.cm.autumn)
 
-for xy0 in [[-2,1],[-0.5,1],[-1.1,1],[-2,-2],[-2,-1],[-2,-3]]:
-    xy = odeint(f, xy0, t)
-    plt.plot(xy[:,0],xy[:,1],c=cs[i%len(cs)],label='({0},{1})'.format(xy0[0],xy0[1]))
-    plt.xlim(-20,20)
+for xy0 in [[-2,3],[-0.5,3],[-1.1,3],[-2,-3],[-2,-3],[-2,-3]]:
+    xy = odeint(ff, xy0, t)
+    plt.plot(xy[:,0],xy[:,1],c=cs[i%len(cs)],label='({0},{1})'.format(xy0[0],xy0[1]),linewidth=3)
+    plt.xlim(-5,5)
     plt.ylim(-3,3)
     plt.title('Example 6.1.1')
     i+=1
