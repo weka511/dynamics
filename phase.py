57c1240cf8f0d3e7e5dd1b9d97b25e2fbeb70a04
Simon Crase <simon@greenweaves.nz>
2017-12-30 09:39:14 +1300

    Upgraded doco

--- phase.py
+++ phase.py
@@ -18,11 +18,10 @@ from matplotlib import rc
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 rc('text', usetex=True)
 
-def ff(x,t,f):
-    u,v=f(x[0],x[1])
-    return [u]+[v]
-
 def generate(f=lambda x,y:(x,y),nx=64, ny = 64,xmin=-10,xmax=10,ymin=-10,ymax=10):
+    '''
+    Generate a gris X,Y and the corresponding derivatives
+    '''
     x = np.linspace(xmin, xmax,nx)
     y = np.linspace(ymin, ymax, ny)
     X, Y = np.meshgrid(x, y)
@@ -31,17 +30,27 @@ def generate(f=lambda x,y:(x,y),nx=64, ny = 64,xmin=-10,xmax=10,ymin=-10,ymax=10
 
 @np.vectorize
 def nullclines(u,v):
-    def y(v,offset=0):
+    '''
+    Used to plot nullclines. Forgets everyting except sign of u and v
+    '''
+    def setnum_offset(v,offset=0):
         return offset if v<0 else offset+1        
-    return y(v) if u<0 else y(v,offset=2)
+    return setnum_offset(v) if u<0 else setnum_offset(v,offset=2)
 
 def plot_phase_portrait(X,Y,U,V,title=''):
+    '''
+    Plot nullclines and steram lines
+    '''
     plt.pcolor(X,Y,nullclines(U,V),cmap=plt.cm.Pastel1)
     plt.streamplot(X, Y, U, V, linewidth=1)
     plt.title(title)
 
 def adapt(f):
-    def adapted(x,y):
+    '''
+    Adapt a 2D function so it is in the form that scipy.integrate.odeint requires, i.e.:
+    ((x,y)->(dx,dy))->(([x],t)->[dx])
+    '''
+    def adapted(x,t):
         u,v=f(x[0],x[1])
         return [u]+[v]
     return adapted
@@ -56,16 +65,15 @@ if __name__=='__main__':
     cs = ['r','b','g','m','c','y']
     X,Y,U,V=generate(f,nx=256, ny = 256)
     plot_phase_portrait(X,Y,U,V,title='Example 6.1.1: $\dot{x}=x+e^{-y},dot{y}=-y$')
-    i=0
-    for xy0 in [[-2,3],[-0.5,3],[-1.1,3],[-2,-3],[-2,-3],[-2,-3]]:
+    starts=[[-2,3],[-0.5,3],[-1.1,3],[-2,-3],[-2,-3],[-2,-3]]
+    for xy0,i in zip(starts,range(len(starts))):
         xy = odeint(adapt(f=f), xy0, t)
         plt.plot(xy[:,0],xy[:,1],c=cs[i%len(cs)],label='({0},{1})'.format(xy0[0],xy0[1]),linewidth=3)
         plt.xlim(-5,5)
         plt.ylim(-3,3)
         plt.xlabel('x')
         plt.ylabel('y')
-        #plt.title('Example 6.1.1')
-        i+=1
+        
     plt.legend(loc='best')    
     
     plt.show()
