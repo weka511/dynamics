528bcf365c6a1d76bf7bedce493cfb4920c49b31
Simon Crase <simon@greenweaves.nz>
2018-01-11 14:04:44 +1300

    As submitted for week 4

--- parityRule.py
+++ parityRule.py
@@ -18,7 +18,7 @@ def readImage(string): # This function only work for monochrome BMP.
 # Main Program
 
 # Program input, i.e. the name of the image "imageName" and the maximum number of iteration "maxIter"
-imageName = 'image1.bmp'
+imageName = 'image3.bmp'
 maxIter   = 32
 
 # Read the image and store it in the array "image"
@@ -32,10 +32,10 @@ imageSize = np.shape(image);
 
 # Print to screen the initial image.
 print('Initial image:')
-plt.clf()
+#plt.clf()
 plt.imshow(image, cmap=cm.gray)
-plt.show()
-plt.pause(0.1)
+plt.figure()
+#plt.pause(0.1)
 
 # Main loop
 for it in range(1,maxIter+1):
@@ -49,11 +49,18 @@ for it in range(1,maxIter+1):
     # Code that you need to write...
     
     # Print to screen the image after each iteration.
+
+    m=len(image)
+    n=len(image[0])
+    for i in range(m):
+        for j in range(n):
+            image[i][j]=(imageCopy[(i-1)%m][j]+imageCopy[(i+1)%m][j]+imageCopy[i][(j-1)%n]+imageCopy[i][(j+1)%n])%2
     print('Image after',it,'iterations:')
     plt.clf()
     plt.imshow(image, cmap=cm.gray)
-    plt.show()
-    plt.pause(0.1)
+    plt.figure()
+    #plt.pause(0.1)
         
 # Print to screen the number of white pixels in the final image
-print("The number of white pixels after",it,"iterations is: ", sum(image))
+print("The number of white pixels after",it,"iterations is: ", sum(sum(image)))
+plt.show()
