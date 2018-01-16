ca6ddd5819c0232074c6509fca632f988dd61cc4
Simon Crase <simon@greenweaves.nz>
2018-01-17 10:04:24 +1300

    Reviewed code and added doco

--- barnes_hut.py
+++ barnes_hut.py
@@ -34,48 +34,59 @@ class Node:
 # or an abstract node of the quad-tree if it has child.
 
     def __init__(self, m, x, y,z):
-    # The initializer creates a child-less node (an actual body).
+        '''
+        The initializer creates a child-less node (an actual body).
+        Instead of storing the position of a node, we store the mass times
+        position, m_pos. This makes it easier to update the center-of-mass.
+        '''
         self.m = m
-        # Instead of storing the position of a node, we store the mass times
-        # position, m_pos. This makes it easier to update the center-of-mass.
-        self.m_pos = m * np.array([x, y,z])
+        self.m_pos = m * np.array([x, y,z])  #mass*position
         self.momentum = np.array([0., 0.,0.])
         self.child = None
 
     def into_next_octant(self):
-    # Places node into next-level octant and returns the octant number.
+        '''
+        Place node into next-level octant and return the octant number.
+        '''
         self.s = 0.5 * self.s   # s: side-length of current octant.
         return self._subdivide(2) + 2*self._subdivide(1) + 4*self._subdivide(0)
 
     def pos(self):
-    # Physical position of node, independent of currently active octant.
+        '''
+        Physical position of node, independent of currently active octant.
+        '''
         return self.m_pos / self.m
 
     def reset_to_0th_octant(self):
-    # Re-positions the node to the level-0 octant (full domain).
-        # Side-length of the level-0 octant is 1.
+        '''
+        Re-position the node to the level-0 octant (full domain).
+        Side-length of the level-0 octant is 1.
+        '''
         self.s = 1.0
-        # Relative position inside the octant is equal to physical position.
-        self.relpos = self.pos().copy()
+        self.relpos = self.pos().copy() # Relative position inside the octant is equal to physical position.
 
     def dist(self, other):
-    # Distance between present node and another node.
+        '''
+        Distance between present node and another node.
+        '''
         return np.linalg.norm(other.pos() - self.pos())
 
     def force_on(self, other):
-    # Force which the present node is exerting on a given body.
-        # To avoid numerical instabilities, introduce a short-distance cutoff.
-        cutoff_dist = 0.002
+        '''
+        Force which the present node is exerting on a given body.
+        '''       
+        cutoff_dist = 0.002 # To avoid numerical instabilities, introduce a short-distance cutoff.
         d = self.dist(other)
         if d < cutoff_dist:
-            return np.array([0., 0.,0.])
-        else:
-            # Gravitational force goes like 1/r**2.
+            return np.array([0., 0., 0.])
+        else: # Gravitational force goes like 1/r**2           
             return (self.pos() - other.pos()) * (self.m*other.m / d**3)
 
     def _subdivide(self, i):
-    # Places node into next-level octant along direction i and recomputes
-    # the relative position relpos of the node inside this octant.
+        '''
+        Place node into next-level octant along direction i and recompute
+        the relative position relpos of the node inside this octant.
+        '''
         self.relpos[i] *= 2.0
         if self.relpos[i] < 1.0:
             octant = 0
@@ -86,10 +97,13 @@ class Node:
 
 
 def add(body, node):
-# Barnes-Hut algorithm: Creation of the quad-tree. This function adds
-# a new body into a quad-tree node. Returns an updated version of the node.
+    '''
+    Barnes-Hut algorithm: Creation of the quad-tree. This function adds
+    a new body into a quad-tree node. Returns an updated version of the node.
+    '''
     # 1. If node n does not contain a body, put the new body b here.
     new_node = body if node is None else None
+    
     # To limit the recursion depth, set a lower limit for the size of octant.
     smallest_octant = 1.e-4
     if node is not None and node.s > smallest_octant:
@@ -118,10 +132,12 @@ def add(body, node):
 
 
 def force_on(body, node, theta):
-# Barnes-Hut algorithm: usage of the quad-tree. This function computes
-# the net force on a body exerted by all bodies in node "node".
-# Note how the code is shorter and more expressive than the human-language
-# description of the algorithm.
+    '''
+    Barnes-Hut algorithm: usage of the quad-tree. This function computes
+    the net force on a body exerted by all bodies in node "node".
+    Note how the code is shorter and more expressive than the human-language
+    description of the algorithm.
+    '''
     # 1. If the current node is an external node, 
     #    calculate the force exerted by the current node on b.
     if node.child is None:
@@ -137,7 +153,9 @@ def force_on(body, node, theta):
 
 
 def verlet(bodies, root, theta, G, dt):
-# Execute a time iteration according to the Verlet algorithm.
+    '''
+    Execute a time iteration according to the Verlet algorithm.
+    '''
     for body in bodies:
         force = G * force_on(body, root, theta)
         body.momentum += dt * force
@@ -145,9 +163,11 @@ def verlet(bodies, root, theta, G, dt):
 
 
 def plot_bodies3(bodies, i):
-# Write an image representing the current position of the bodies.
-# To create a movie with avconv or ffmpeg use the following command:
-# ffmpeg -r 15 -i bodies3D_%06d.png -q:v 0 bodies3D.avi
+    '''
+    Write an image representing the current position of the bodies.
+    To create a movie with avconv or ffmpeg use the following command:
+    ffmpeg -r 15 -i bodies3D_%06d.png -q:v 0 bodies3D.avi
+    '''
     ax = plt.gcf().add_subplot(111, aspect='equal', projection='3d')
     ax.scatter([b.pos()[0] for b in bodies], \
     [b.pos()[1] for b in bodies], [b.pos()[2] for b in bodies])
@@ -156,7 +176,6 @@ def plot_bodies3(bodies, i):
     ax.set_zlim([0., 1.0])    
     plt.gcf().savefig('{0}bodies3D_{1:06}.png'.format(images,i))
     
-######### MAIN PROGRAM ########################################################
 
 # Theta-criterion of the Barnes-Hut algorithm.
 theta = 0.5
@@ -174,41 +193,43 @@ dt = 1.e-3
 # outside the initial radius are removed).
 numbodies = 1000
 # Number of time-iterations executed by the program.
-max_iter = 10000
+max_iter = 501#10000
 # Frequency at which PNG images are written.
 img_iter = 20
 
-# The pseudo-random number generator is initialized at a deterministic # value, for proper validation of the output for the exercise series.  random.seed(1)
-# x- and y-pos are initialized to a square with side-length 2*ini_radius.
-np.random.seed(1)
-posx = np.random.random(numbodies) *2.*ini_radius + 0.5-ini_radius
-posy = np.random.random(numbodies) *2.*ini_radius + 0.5-ini_radius
-posz = np.random.random(numbodies) *2.*ini_radius + 0.5-ini_radius
-
-# We only keep the bodies inside a circle of radius ini_radius.
-bodies = [ Node(mass, px, py,pz) for (px,py,pz) in zip(posx, posy,posz) \
-               if (px-0.5)**2 + (py-0.5)**2 + (pz-0.5)**2< ini_radius**2 ]
-
-
-# Initially, the bodies have a radial velocity of an amplitude proportional to
-# the distance from the center. This induces a rotational motion creating a
-# "galaxy-like" impression.
-for body in bodies: 
-    r = body.pos() - np.array([0.5, 0.5, body.pos()[2] ])
-    body.momentum = np.array([-r[1], r[0], 0.]) *  mass*inivel*np.linalg.norm(r)/ini_radius
-
-# Principal loop over time iterations.
-for i in range(max_iter):
-    # The oct-tree is recomputed at each iteration.
-    root = None
-    for body in bodies:
-        body.reset_to_0th_octant()
-        root = add(body, root)
-    # Compute forces and advance bodies.
-    verlet(bodies, root, theta, G, dt)
-    # Output
-           
-    if i%img_iter==0:
-        print("Writing images at iteration {0}".format(i))
-        plot_bodies3(bodies, i//img_iter)
-        print (i,bodies[0].m_pos[2])
+if __name__=='__main__':
+    # The pseudo-random number generator is initialized at a deterministic # value,
+    # for proper validation of the output for the exercise series.  random.seed(1)
+    # x-, y-pos, and z-pos are initialized to a cube with side-length 2*ini_radius.
+    np.random.seed(1)
+    posx = np.random.random(numbodies) *2.*ini_radius + 0.5-ini_radius
+    posy = np.random.random(numbodies) *2.*ini_radius + 0.5-ini_radius
+    posz = np.random.random(numbodies) *2.*ini_radius + 0.5-ini_radius
+    
+    # We only keep the bodies inside a sphere of radius ini_radius.
+    bodies = [ Node(mass, px, py,pz) for (px,py,pz) in zip(posx, posy,posz) 
+                   if (px-0.5)**2 + (py-0.5)**2 + (pz-0.5)**2< ini_radius**2 ]
+    
+    
+    # Initially, the bodies have a radial velocity of an amplitude proportional to
+    # the distance from the center. This induces a rotational motion creating a
+    # "galaxy-like" impression.
+    for body in bodies: 
+        r = body.pos() - np.array([0.5, 0.5, body.pos()[2] ])
+        body.momentum = np.array([-r[1], r[0], 0.]) *  mass*inivel*np.linalg.norm(r)/ini_radius
+    
+    # Principal loop over time iterations.
+    for i in range(max_iter):
+        # The oct-tree is recomputed at each iteration.
+        root = None
+        for body in bodies:
+            body.reset_to_0th_octant()
+            root = add(body, root)
+        # Compute forces and advance bodies.
+        verlet(bodies, root, theta, G, dt)
+        # Output
+               
+        if i%img_iter==0:
+            print("Writing images at iteration {0}".format(i))
+            plot_bodies3(bodies, i//img_iter)
+        print ('{0},{1}'.format(i,bodies[0].m_pos[2]))
