ddc8e271287f99a1044a3b58ec3ad01593ac0357
Simon Crase <simon@greenweaves.nz>
2022-01-28 18:47:17 +1300

    Reviewed against assignment

--- chaos_book/hw2/Bakers map.py
+++ chaos_book/hw2/Bakers map.py
@@ -14,17 +14,13 @@
 # along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/
 
 def f(pt):
-    x=pt[0]
-    y=pt[1]
-    if y<=2:
-        return (x/3,2*y)
-    else:
-        return (x/3+1/2,2*y-1)
+    x,y = pt
+    return (x/3,2*y) if y<=2  else (x/3+1/2,2*y-1)
 
 def area(u):
     return 0.5*(  u[0][0]*u[1][1] + u[1][0]*u[2][1] + u[2][0]*u[3][1] + u[3][0]*u[0][1]
                 - u[1][0]*u[0][1] - u[2][0]*u[1][1] - u[3][0]*u[2][1] - u[0][0]*u[3][1])
-    
+
 u = [(0,0), (1,0), (1,1), (0,1)]
 
 v = [f(pt) for pt in u]
