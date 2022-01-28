f021c1bb72af86b94b1fa9664654e9e2f499e42f
Simon Crase <simon@greenweaves.nz>
2022-01-28 20:22:59 +1300

    Code review

--- chaos_book/hw2/floquet.py
+++ chaos_book/hw2/floquet.py
@@ -13,28 +13,33 @@
 # You should have received a copy of the GNU General Public License
 # along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/
 
-# Q1.2 A limit cycle with analytic Floquet exponent
+''' Q1.2 A limit cycle with analytic Floquet exponent'''
 
-import sys
-sys.path.append('../../')
-import  matplotlib.pyplot as plt,matplotlib.colors as colors,phase,numpy as np,rk4
+from sys import path
+from matplotlib.pyplot import figure, savefig,show
+from phase import generate,plot_phase_portrait,plot_stability
+
 
 def floquet(q,p):
     return (p+q*(1-q*q-p*p),-q+p*(1-q*q-p*p))
 
-plt.figure(figsize=(20,20))
-X,Y,U,V,fixed_points=phase.generate(f=floquet,
-                             nx=256, ny = 256,
-                             xmin=-1.2,xmax=1.2,ymin=-1.2,ymax=1.2)
+figure(figsize=(20,20))
+X,Y,U,V,fixed_points = generate(f=floquet,
+                                nx   = 256,
+                                ny   = 256,
+                                xmin = -1.2,
+                                xmax = 1.2,
+                                ymin = -1.2,
+                                ymax = 1.2)
 
-phase.plot_phase_portrait(X,Y,U,V,fixed_points,title=r'$\dot{p}=p+q(1-q^2-p^2),\dot{q}=-q+p(1-q^2-p^2)$',
+plot_phase_portrait(X,Y,U,V,fixed_points,title=r'$\dot{p}=p+q(1-q^2-p^2),\dot{q}=-q+p(1-q^2-p^2)$',
                           suptitle='Q1.2 A limit cycle with analytic Floquet exponent.'
                           ' (ChaosBook.org version 14.5.7, exercise 5.1)',
                           xlabel='$p$',
                           ylabel='$q$')
 
-phase.plot_stability(f=floquet,fixed_points=fixed_points,Limit=0.99,step=0.1,N=50000,R=0.01)
+plot_stability(f=floquet,fixed_points=fixed_points,Limit=0.99,step=0.1,N=50000,R=0.01)
 
-plt.savefig('floquet.png')
+savefig('floquet.png')
 
-plt.show()
+show()
