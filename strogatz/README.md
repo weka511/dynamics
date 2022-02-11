a86879cd911adf4ebd91b4035056af8d4699e6cd
Simon Crase <simon@greenweaves.nz>
2022-02-12 12:15:10 +1300

    Documented 3.7.6

--- strogatz/README.md
+++ strogatz/README.md
@@ -4,16 +4,15 @@
 
 -| Name | Description
 --:|------------------|----------------------------------------------------------
-3|exercise_3.7.6.py|
-6| exercise_6_1.py |Plot phase portraits for a number of ODEs
-6| exercise_6_2.py |Plot phase portraits for a number of ODEs
-6| exercise_6_3.py |Plot phase portraits for a number of ODEs
-6| exercise_6_3_10.py |Stability of fixed point: saddle or not? (tricky)
-6| exercise_6_4.py |Plot phase portraits for Competitive Lotka–Volterra equations
-6| exercise_6_4_4.py |Plot phase portraits for simplified Competitive Lotka–Volterra equations
-6| exercise_6_4_8.py |Plot phase portraits for evolutionary dynamics
-6| exercise_6_5_19.py |Plot phase portraits for Lotka–Volterra predator prey model
-8| example_8_7_1|Show limit cycle of example 8.7.1
+3|exercise_3.7.6.py|Kermack & McKendrick model of an epidemic
+6|exercise_6_1.py|Plot phase portraits for a number of ODEs
+6|exercise_6_2.py|Plot phase portraits for a number of ODEs
+6|exercise_6_3.py|Plot phase portraits for a number of ODEs
+6|exercise_6_3_10.py|Stability of fixed point: saddle or not? (tricky)
+6|exercise_6_4.py|Plot phase portraits for Competitive Lotka–Volterra equations
+6|exercise_6_4_4.py|Plot phase portraits for simplified Competitive Lotka–Volterra equations
+6|exercise_6_4_8.py|Plot phase portraits for evolutionary dynamics
+6|exercise_6_5_19.py|Plot phase portraits for Lotka–Volterra predator prey model
+8|example_8_7_1|Showlimit cycle of example 8.7.1
 8|exercises_8.tex|Theoretical Exercises from Strogatz
-10| exercise_10_6.py |Bifurcations
-
+10|exercise_10_6.py|Bifurcations
--- strogatz/exercise_3_7_6.py
+++ strogatz/exercise_3_7_6.py
@@ -1,23 +1,58 @@
-import matplotlib.pyplot as plt
-import math
+
+# Copyright (C) 2017-2022 Greenweaves Software Limited
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
+
+'''Kermack & McKendrick model of an epidemic'''
+from matplotlib.pyplot import figure, grid, legend, plot,  show,  title
+from math              import exp
+
 
 aa      = [1,2,3]
 bs      = [0.1,0.2,0.3]
 h       = 0.1
 l       = 10.0
-colours = ['r','g','b','c','m','y']
-i       = 0
+colours = ['xkcd:purple',
+           'xkcd:green',
+           'xkcd:blue',
+           'xkcd:pink',
+           'xkcd:brown',
+           'xkcd:red',
+           'xkcd:light blue',
+           'xkcd:teal',
+           'xkcd:orange'
+           ]
+
 
 us      = [h*u for u in range(0,int(l/h))]
-u2s     = [math.exp(-u) for u in us]
-plt.plot(us,u2s,c='k')
+u2s     = [exp(-u) for u in us]
 
-for a in aa:
-    for b in bs:
+figure(figsize = (10,10))
+
+plot(us,u2s,
+     c     = 'xkcd:black',
+     label = r'$e^{-u}$')
+
+for i,a in enumerate(aa):
+    for j,b in enumerate(bs):
         u1s = [a - b *u for u in us]
-        plt.plot(us,u1s,c=colours[i],label='a={0},b={1}'.format(a,b))
-        i+=1
-        i%=len(colours)
-        
-plt.legend()
-plt.show()
+        plot(us,u1s,
+             c     = colours[len(bs)*i+j],
+             label = 'a={0},b={1}'.format(a,b))
+
+
+title('Kermack & McKendrick model of an epidemic')
+grid(True)
+legend()
+show()
