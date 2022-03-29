cb245c3701f49fbe0b9dd4859ba83033c8f0cd2a
Simon Crase <simon@greenweaves.nz>
2022-03-30 08:43:10 +1300

    Plot inslice

--- chaos_book/wk5/relativeP.py
+++ chaos_book/wk5/relativeP.py
@@ -1,27 +1,44 @@
 '''
    Relative periodic orbits from Table 12.1
 '''
-from matplotlib.pyplot import show
+
 from numpy             import arange, array
 from scipy.integrate   import odeint
-from twoModes          import MultiPlotter, reduceSymmetry, velocity
+from twoModes          import MultiPlotter, reduceSymmetry, velocity, velocity_reduced
 
 
 def get_orbit(x0,period,dtau = 0.005):
    return odeint(velocity, x0, arange(0, period, dtau))
 
+def get_orbit_reduced(x0,period,dtau = 0.005):
+   return odeint(velocity_reduced, reduceSymmetry(x0), arange(0, period, dtau))
+
 if __name__ == '__main__':
-   with MultiPlotter(name='periodic') as plt1,\
-        MultiPlotter(name='reduced') as plt2:
-      orbit1 = get_orbit(array([0.4525719, 0.0, 0.0509257, 0.0335428]), 3.6415120)
-      orbit2 = get_orbit(array([0.4517771, 0.0, 0.0202026, 0.0405222]), 7.3459412)
-      orbit3 = get_orbit(array([0.4514665, 0.0, 0.0108291, 0.0424373]), 14.6795175)
-      orbit4 = get_orbit(array([0.4503967, 0.0, -0.0170958, 0.0476009]), 18.3874094)
-      plt1.plot(orbit1[:,0:3], title='1')
-      plt1.plot(orbit2[:,0:3], title='01')
-      plt1.plot(orbit3[:,0:3], title='0111')
-      plt1.plot(orbit4[:,0:3], title='01101')
-      plt2.plot(reduceSymmetry(orbit1)[:,0:3], title='1')
-      plt2.plot(reduceSymmetry(orbit2)[:,0:3], title='01')
-      plt2.plot(reduceSymmetry(orbit3)[:,0:3], title='0111')
-      plt2.plot(reduceSymmetry(orbit4)[:,0:3], title='01101')
+   with MultiPlotter(name='periodic') as plt1,   \
+        MultiPlotter(name='reduced')  as plt2,   \
+        MultiPlotter(name='inslice')  as plt3:
+
+      orbit1   = get_orbit(array([0.4525719, 0.0, 0.0509257, 0.0335428]), 3.6415120)
+      orbit2   = get_orbit(array([0.4517771, 0.0, 0.0202026, 0.0405222]), 7.3459412)
+      orbit3   = get_orbit(array([0.4514665, 0.0, 0.0108291, 0.0424373]), 14.6795175)
+      orbit4   = get_orbit(array([0.4503967, 0.0, -0.0170958, 0.0476009]), 18.3874094)
+
+      inslice1 = get_orbit_reduced(array([0.4525719, 0.0, 0.0509257, 0.0335428]), 3.6415120)
+      inslice2 = get_orbit_reduced(array([0.4517771, 0.0, 0.0202026, 0.0405222]), 7.3459412)
+      inslice3 = get_orbit_reduced(array([0.4514665, 0.0, 0.0108291, 0.0424373]), 14.6795175)
+      inslice4 = get_orbit_reduced(array([0.4503967, 0.0, -0.0170958, 0.0476009]), 18.3874094)
+
+      plt1.plot(orbit1[:,0:3], title = '1')
+      plt1.plot(orbit2[:,0:3], title = '01')
+      plt1.plot(orbit3[:,0:3], title = '0111')
+      plt1.plot(orbit4[:,0:3], title = '01101')
+
+      plt2.plot(reduceSymmetry(orbit1)[:,0:3], title = '1')
+      plt2.plot(reduceSymmetry(orbit2)[:,0:3], title = '01')
+      plt2.plot(reduceSymmetry(orbit3)[:,0:3], title = '0111')
+      plt2.plot(reduceSymmetry(orbit4)[:,0:3], title = '01101')
+
+      plt3.plot(inslice1, title = '1')
+      plt3.plot(inslice2, title = '01')
+      plt3.plot(inslice3, title = '01111')
+      plt3.plot(inslice4, title = '01101')
