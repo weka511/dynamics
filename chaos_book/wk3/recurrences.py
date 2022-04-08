5bdb81d4b2d17d899025fa8d3cef3d4a06aba8e0
Simon Crase <simon@greenweaves.nz>
2022-04-08 15:55:08 +1200

    Started plotting details of Poincare Sections

--- chaos_book/wk3/recurrences.py
+++ chaos_book/wk3/recurrences.py
@@ -8,28 +8,25 @@ from scipy.interpolate      import splev,  splprep, splrep
 from scipy.spatial.distance import pdist, squareform
 
 
-def create_trajectory(epsilon  =1e-6,
+def create_trajectory(epsilon  = 1e-6,
                       tInitial = 0,
                       tFinal   = 1000,
                       Nt       = 100000):
     '''
-    We will first run a long trajectory of the Rossler system by starting
-    close to the eq0 in order to include its unstable manifold on the Poincare
-    section. Let us start by repeating what we have done in the stability
-    exercise and construct this initial condition:
-    Numerically find the equilibrium of the Rossler system close to the
-    origin
+    Create a long trajectory of the Rossler system by starting lose to the eq0
+    in order to include its unstable manifold on the Poincare
+    section.
     '''
-    eq0                       = fsolve(Velocity, array([0, 0, 0], float), args=(0,))
-    Aeq0                      = StabilityMatrix(eq0)
-    eigenValues, eigenVectors = eig(Aeq0)
-    v1                        = real(eigenVectors[:, 0]) #Real part of the leading eigenvector
-    v1                        = v1 / norm(v1)
-    ssp0                      = eq0 + epsilon * v1  #Initial condition as a slight perturbation to the eq0 in v1 direction
+    def get_normalized_real(V):
+        '''Used to get normalized real part of a vector'''
+        v1 = real(V)
+        return v1 / norm(v1)
 
-    tArray                    = linspace(tInitial, tFinal, Nt)
-    sspSolution               = odeint(Velocity, ssp0, tArray)
-    return tArray, sspSolution
+    # Find the equilibrium of the Rossler system close to the origin
+    eq0             = fsolve(Velocity, array([0, 0, 0], float), args = (0,))
+    _, eigenVectors = eig(StabilityMatrix(eq0))
+    tArray          = linspace(tInitial, tFinal, Nt)
+    return tArray, odeint(Velocity,  eq0 + epsilon * get_normalized_real(eigenVectors[:, 0]), tArray)
 
 class UPoincare:
     '''
@@ -55,11 +52,12 @@ class UPoincare:
         '''
         Define vectors which will be on and orthogonal to the Poincare section hyperplane:
         '''
-        e_x         = array([1, 0, 0], float)  # Unit vector in x-direction
-        sspTemplate = dot(UPoincare.zRotation(thetaPoincare), e_x)  #Template vector to define the Poincare section hyperplane
-        nTemplate   = dot(UPoincare.zRotation(pi/2), sspTemplate)  #Normal to this plane will be equal to template vector rotated pi/2 about the z axis
+        e_x              = array([1, 0, 0], float)  # Unit vector in x-direction
+        sspTemplate      = dot(UPoincare.zRotation(thetaPoincare), e_x)  #Template vector to define the Poincare section hyperplane
+        nTemplate        = dot(UPoincare.zRotation(pi/2), sspTemplate)  #Normal to this plane will be equal to template vector rotated pi/2 about the z axis
         self.sspTemplate = sspTemplate
         self.nTemplate   = nTemplate
+        self.e_z         = array([0, 0, 1], float)
 
     def UPoincare(self,ssp):
         '''
@@ -74,18 +72,53 @@ class UPoincare:
 
         return dot((ssp - self.sspTemplate) , self.nTemplate)
 
+    def do_stuff(self,sspSolutionPoincare):
+        ProjPoincare = array([self.sspTemplate,
+                              self.e_z,
+                              self.nTemplate], float)
+        #sspSolutionPoincare has column vectors on its rows. We act on the
+        #transpose of this matrix to project each state space point onto Poincare
+        #basis by a simple matrix multiplication:
+        PoincareSection = dot(ProjPoincare, sspSolutionPoincare.transpose())
+        PoincareSection =  PoincareSection.transpose()   #We return to the usual N x 3 form by another transposition
+        #Third column of this matrix should be zero if everything is correct, so we
+        #discard it:
+
+        PoincareSection = PoincareSection[:, 0:2]
+        return PoincareSection
+
+    def foo(self,PoincareSection):
+        Distance = squareform(pdist(PoincareSection))
+        SortedPoincareSection = PoincareSection.copy()  # Copy PoincareSection into
+                                                        # a new variable
+        #Create a zero-array to assign arclengths of the Poincare section points
+        #after sorting:
+        ArcLengths = zeros(size(SortedPoincareSection, 0))
+        # Create another zero-array to assign the arclengths of the Poincare section
+        # points keeping their dynamical order for use in the return map
+        sn = zeros(size(PoincareSection, 0))
+        for k in range(size(SortedPoincareSection, 0) - 1):
+            #Find the element which is closest to the kth point:
+            m = argmin(Distance[k, k + 1:]) + k + 1
+            #Hold the (k+1)th row in the dummy vector:
+            dummyPoincare = SortedPoincareSection[k + 1, :].copy()
+            #Replace (k+1)th row with the closest point:
+            SortedPoincareSection[k + 1, :] = SortedPoincareSection[m, :]
+            #Assign the previous (k+1)th row to the mth row:
+            SortedPoincareSection[m, :] = dummyPoincare
+        return SortedPoincareSection
+
 def get_crossing(sspSolution,i,upoincare,tArray):
     '''
     find where exactly the flow pierces the Poincare section:
     '''
-
     return Flow(sspSolution[i],
                 fsolve(lambda deltat: upoincare.UPoincare(Flow(sspSolution[i] , deltat)),
                                  (tArray[i + 1] - tArray[i]) / 2) )    #Now integrate deltat from sspPoincare0 to find where exactly the
                                                        #flow pierces the Poincare section:
 
-def create_section(thetaPoincare,tArray, sspSolution):
-    upoincare   = UPoincare(thetaPoincare)
+
+def create_section(upoincare,tArray, sspSolution):
 
     return array([get_crossing(sspSolution,i,upoincare,tArray)
                   for i in range(size(sspSolution, 0) - 1)
@@ -93,10 +126,12 @@ def create_section(thetaPoincare,tArray, sspSolution):
                  float)
 
 if __name__=='__main__':
-    Angles              = [0, 60, 120, -60]
+    Angles              = [-60, 0, 60, 120]
     tArray, sspSolution = create_trajectory()
-
-    PoincareSections    = [create_section((angle/60) * (pi/3),tArray, sspSolution) for angle in Angles]
+    UPoincares          = [UPoincare((angle/60) * (pi/3))  for angle in Angles]
+    PoincareSections    = [create_section(upoincare,tArray, sspSolution) for upoincare in UPoincares]
+    PoincareSections2   = [upoincare.do_stuff(sspSolutionPoincare) for upoincare,sspSolutionPoincare in zip(UPoincares,PoincareSections)]
+    PoincareSectionsS   = [upoincare.foo(X) for upoincare,X in zip(UPoincares,PoincareSections2)]
 
     fig                 = figure(figsize=(12,12))
     ax                  = fig.gca(projection='3d')
@@ -121,4 +156,16 @@ if __name__=='__main__':
     ax.set_title('Poincare Recurrences for Rossler')
     ax.legend( prop={'family': 'monospace'})
     savefig('recurrences')
+
+    fig = figure(figsize=(12,12))
+    axes = fig.subplots(nrows = 3,
+                        ncols = len(Angles))
+    for i in range(len(Angles)):
+        axes[0][i].plot(PoincareSections2[i][:, 0], PoincareSections2[i][:, 1], styles[i],
+                markersize = 5,
+                label      = 'Poincare Section')
+        axes[0][i].set_title(f'{Angles[i]}')
+        axes[1][i].plot(PoincareSectionsS[i][:, 0], PoincareSectionsS[i][:, 1], styles[i],
+                markersize = 5,
+                label      = 'Poincare Section')
     show()
