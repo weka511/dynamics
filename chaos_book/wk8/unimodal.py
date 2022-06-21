b6012742650630e4e5c412a9a4ddedcc64caf4df
Simon Crase <simon@greenweaves.nz>
2022-06-21 19:25:11 +1200

    Simplified logic

--- chaos_book/wk8/unimodal.py
+++ chaos_book/wk8/unimodal.py
@@ -1,41 +1,39 @@
 from numpy import array, append
 
 class Unimodal:
-    """
+    '''
     symbolic dynamics of a unimodal map.
     input parameters:
-         returnMap: the retur map itself
+         returnMap: the return map itself
          C : critical point of this map
-    """
+    '''
     def __init__(self, returnMap, C):
         self.returnMap = returnMap
-        self.C = C
+        self.C         = C
 
     def returnMap_iter(self, x0, n):
-        """
+        '''
         multiple iterations of this map from point x0
         n: the iteration number.
-        """
-        x = x0
+        '''
+        x      = x0
         future = array([])
         for i in range(n):
-            x = self.returnMap(x)
+            x      = self.returnMap(x)
             future = append(future, x)
 
         return future
 
     def future_symbol(self, x0, n):
-        """
+        '''
         future itinerary of point x0.
         n : the iteration number.
-        """
-        x = x0
+        '''
+        x      = x0
         future = array([])
         for i in range(n):
-            x = self.returnMap(x);
-            if x > self.C: symbol = 1
-            else: symbol = 0
-            future = append(future, symbol)
+            x      = self.returnMap(x)
+            future = append(future, int(x > self.C))
 
         return future
 
