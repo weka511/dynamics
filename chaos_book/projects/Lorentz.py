5320b50a2286770f85e4c6fe89a27bd92785fed4
Simon Crase <simon@greenweaves.nz>
2022-07-20 19:04:54 +1200

    Started on Poincare Section

--- chaos_book/projects/Lorentz.py
+++ chaos_book/projects/Lorentz.py
@@ -1,6 +1,6 @@
 from argparse          import ArgumentParser
 from matplotlib.pyplot import figure, savefig, show, suptitle
-from numpy             import arange, array, sqrt
+from numpy             import arange, array, cos, dot, pi, sin, sqrt
 from numpy.random      import rand
 from os.path           import join, basename, split
 from pathlib           import Path
@@ -21,10 +21,6 @@ class Dynamics:
     def get_z_label(self):
         return 'z'
 
-sigma = 10.0
-rho   = 28.0
-b     = 8.0/3.0
-figs  = './figs'
 
 class Lorentz(Dynamics):
     def __init__(self,
@@ -57,9 +53,9 @@ class Lorentz(Dynamics):
         y = stateVec[1]
         z = stateVec[2]
 
-        return array([sigma * (y-x),
-                      rho*x - y - x*z,
-                      x*y - b*z])
+        return array([self.sigma * (y-x),
+                      self.rho*x - y - x*z,
+                      x*y - self.b*z])
 
 class PseudoLorentz(Dynamics):
     def __init__(self,
@@ -70,6 +66,11 @@ class PseudoLorentz(Dynamics):
         self.rho   = rho
         self.b     = b
         self.name     = 'Pseudo Lorentz'
+
+    def create_eqs(self):
+        eq0 = [0,0,0]
+        eq1 = [0,2*self.b*(self.rho-1),self.rho-1]
+        return  array([eq0,eq1])
 
     def velocity(self,t,stateVec):
         u = stateVec[0]
@@ -101,75 +102,55 @@ class Integrator:
         return : a [ nstp x 3 ] vector
         '''
 
-        return solve_ivp(dynamics.velocity, (0, dt), init_x, t_eval=arange(0,dt,dt/nstp)).y
+        bunch = solve_ivp(dynamics.velocity, (0, dt), init_x, t_eval=arange(0,dt,dt/nstp))
+        if bunch.status==0:
+            return bunch.t, bunch.y
+        else:
+            raise(Exception(f'{bunch.status} {bunch.message}'))
 
-def stabilityMatrix(stateVec):
-    '''
-    return the stability matrix at a state point.
-    stateVec: the state vector in the full space. [x, y, z]
-    '''
+class PoincareSection:
+    @staticmethod
+    def zRotation(theta):
+        '''
+        Rotation matrix about z-axis
+        Input:
+        theta: Rotation angle (radians)
+        Output:
+        Rz: Rotation matrix about z-axis
+        '''
+        return array([[cos(theta), -sin(theta), 0],
+                      [sin(theta), cos(theta),  0],
+                      [0,          0,           1]],
+                     float)
 
-    x = stateVec[0]; y = stateVec[1]; z = stateVec[2];
-    # fill out the following matrix.
-    stab = array([
-            [-sigma, sigma, 0],
-            [rho-z,  -1 , -x],
-            [y,      x,   -b]
-            ])
+    def __init__(self,theta=0.0):
+        e_x              = array([1, 0, 0], float)  # Unit vector in x-direction
+        self.sspTemplate = dot(PoincareSection.zRotation(theta), e_x)  #Template vector to define the Poincare section hyperplane
+        self.nTemplate   = dot(PoincareSection.zRotation(pi/2), self.sspTemplate)  #Normal to this plane will be equal to template vector rotated pi/2 about the z axis
 
-    return stab
+    def UPoincare(self, ssp):
+        '''
+        Plane equation for the Poincare section hyperplane which includes z-axis
+        and makes an angle theta with the x-axis see ChaosBook ver. 14, fig. 3.2
+        Inputs:
+        ssp: State space point at which the Poincare hyperplane equation will be
+             evaluated
+        Outputs:
+        U: Hyperplane equation which should be satisfied on the Poincare section
+           U = (ssp - sspTemplate) . nTemplate (see ChaosBook ver. 14, eq. 3.6)
+        '''
+        return dot((ssp - self.sspTemplate),self.nTemplate)
 
-def integrator(init_x, dt, nstp):
-    '''
-    The integator of the Lorentz system.
-    init_x: the intial condition
-    dt : time step
-    nstp: number of integration steps.
-
-    return : a [ nstp x 3 ] vector
-    '''
-
-    return solve_ivp(velocity, (0, dt), init_x, t_eval=arange(0,dt,dt/nstp)).y
-
-
-def integrator_with_jacob(init_x, dt, nstp):
-    '''
-    integrate the orbit and the Jacobian as well. The meaning
-    of input parameters are the same as 'integrator()'.
-
-    return :
-            state: a [ nstp x 3 ] state vector
-            Jacob: [ 3 x 3 ] Jacobian matrix
-    '''
-    def JacobianVelocity(sspJacobian, t):
-        ssp        = sspJacobian[0:d]                 # First three elements form the original state space vector
-        J          = sspJacobian[d:].reshape((d, d))  # Last nine elements corresponds to the elements of Jacobian.
-        velJ       = zeros(size(sspJacobian))         # Initiate the velocity vector as a vector of same size as sspJacobian
-        velJ[0:d]  = velocity(ssp, t)
-        velTangent = dot(stabilityMatrix(ssp), J)     # Velocity matrix for  the tangent space
-        velJ[d:]   = reshape(velTangent, d*d)           # Last dxd elements of the velJ are determined by the action of
-                                                      # stability matrix on the current value of the Jacobian:
-        return velJ
-
-    d                   = len(init_x)
-    Jacobian0           = identity(d)
-    sspJacobian0        = zeros(d + d ** 2)
-    sspJacobian0[0:d]   = init_x
-    sspJacobian0[d:]    = reshape(Jacobian0, d**2)
-
-    sspJacobianSolution = solve_ivp(JacobianVelocity,    #FIXME
-                                 arange(0, dt*nstp, dt),
-                                 sspJacobian0)
-    state = sspJacobianSolution[0:d]
-    Jacob = sspJacobianSolution[-1, d:].reshape((d, d))
-
-    return state, Jacob
-
-
+    def interections(self,ts, orbit):
+        _,n = orbit.shape
+        for i in range(n-1):
+            if self.UPoincare(orbit[:,i])<0 and self.UPoincare(orbit[:,i+1])>0:
+                yield(0.5+(ts[i]+ts[i+1]), orbit[:,i])
 
 if __name__ == '__main__':
     parser        = ArgumentParser()
-    parser.add_argument('action', type=int)
+    parser.add_argument('action', type = int)
+    parser.add_argument('--figs', default = './figs')
     args          = parser.parse_args()
     fig = figure(figsize=(12,12))
 
@@ -180,15 +161,16 @@ if __name__ == '__main__':
         x0            = EQs[0,:] + 0.001*rand(3)
         dt            = 0.005
         nstp          = 50.0/dt
-        orbit         = integrator.integrate(x0, 50.0, nstp)
-
+        ts,orbit      = integrator.integrate(x0, 50.0, nstp)
+        section       = PoincareSection()
         ax  = fig.add_subplot(111, projection='3d')
         ax.plot(orbit[0,:], orbit[1,:], orbit[2,:],
                 markersize = 1)
         ax.scatter(EQs[0,0], EQs[0,1], EQs[0,2], marker='o', c='xkcd:red', label='EQ0')
         ax.scatter(EQs[1,0], EQs[1,1], EQs[1,2], marker='1', c='xkcd:red', label='EQ1')
         ax.scatter(EQs[2,0], EQs[2,1], EQs[2,2], marker='2',c='xkcd:red', label='EQ2')
-        ax.set_title(fr'Lorentz $\sigma=${sigma}, $\rho=${rho}, b={b}')
+        for t,point in section.interections(ts,orbit):
+            ax.scatter(point[0],point[1],point[2],c='xkcd:green')
         ax.set_title(dynamics.get_title())
         ax.set_xlabel(dynamics.get_x_label())
         ax.set_ylabel(dynamics.get_y_label())
@@ -196,20 +178,19 @@ if __name__ == '__main__':
         ax.legend()
 
     if args.action==2:
-        dynamics = PseudoLorentz()
+        dynamics   = PseudoLorentz()
         integrator = Integrator(dynamics)
-        EQs           = create_eqs()
-        x =  EQs[0,0]
-        y =  EQs[0,1]
-        z =  EQs[0,2]
-        x0            = array([x**2-y**2, 2*x*y,z]) + 0.001*rand(3)
-        dt            = 0.005
-        nstp          = 50.0/dt
-        orbit = integrator.integrate(x0, 50.0, nstp)
+        EQs        = dynamics.create_eqs()
+        x0         = EQs[0,:] + 0.001*rand(3)
+        dt         = 0.001
+        nstp       = 50.0/dt
+        ts,orbit = integrator.integrate(x0, 50.0, nstp)
 
         ax  = fig.add_subplot(111, projection='3d')
         ax.plot(orbit[0,:], orbit[1,:], orbit[2,:],
                 markersize = 1)
+        ax.scatter(EQs[0,0], EQs[0,1], EQs[0,2], marker='o', c='xkcd:red', label='EQ0')
+        ax.scatter(EQs[1,0], EQs[1,1], EQs[1,2], marker='1', c='xkcd:red', label='EQ1')
 
         ax.set_title(dynamics.get_title())
         ax.set_xlabel(dynamics.get_x_label())
@@ -218,5 +199,5 @@ if __name__ == '__main__':
 
         ax.legend()
 
-    savefig(join(figs,f'{Path(__file__).stem}{args.action}'))
+    savefig(join(args.figs,f'{Path(__file__).stem}{args.action}'))
     show()
