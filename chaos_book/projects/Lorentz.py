a48cafba17c400781995f02ca2539956c68a7ba1
Simon Crase <simon@greenweaves.nz>
2022-07-21 16:10:12 +1200

    Poincare Section now uses s fix point as a template

--- chaos_book/projects/Lorentz.py
+++ chaos_book/projects/Lorentz.py
@@ -1,5 +1,6 @@
+'''Investigation of Lorentz Equations'''
 from argparse          import ArgumentParser
-from matplotlib.pyplot import figure, savefig, show, suptitle
+from matplotlib.pyplot import figure, rcParams, savefig, show, suptitle
 from numpy             import arange, array, cos, dot, pi, sin, sqrt
 from numpy.random      import rand
 from os.path           import join, basename, split
@@ -109,7 +110,14 @@ class Integrator:
         else:
             raise(Exception(f'{bunch.status} {bunch.message}'))
 
+
+
+    def Flow(self,deltat,y):
+        bunch = solve_ivp(dynamics.velocity, (0, deltat), y)
+        return bunch.t[1],bunch.y[:,1]
+
 class PoincareSection:
+    ''' This class represents a Poincare Section'''
     @staticmethod
     def zRotation(theta):
         '''
@@ -120,15 +128,18 @@ class PoincareSection:
         Rz: Rotation matrix about z-axis
         '''
         return array([[cos(theta), -sin(theta), 0],
-                      [sin(theta), cos(theta),  0],
+                      [sin( theta), cos(theta),  0],
                       [0,          0,           1]],
                      float)
 
-    def __init__(self,dynamics,theta=0.0):
-        e_x              = array([1, 0, 0], float)  # Unit vector in x-direction
-        self.sspTemplate = dot(PoincareSection.zRotation(theta), e_x)  #Template vector to define the Poincare section hyperplane
-        self.nTemplate   = dot(PoincareSection.zRotation(pi/2), self.sspTemplate)  #Normal to this plane will be equal to template vector rotated pi/2 about the z axis
-        self.integrator  = Integrator(dynamics)
+    def __init__(self,dynamics,integrator,
+                 sspTemplate = None,
+                 nTemplate   = None,
+                 theta       = 0.0,
+                 e_x         = array([1, 0, 0], float)):
+        self.sspTemplate = dot(PoincareSection.zRotation(theta), e_x)  if len(sspTemplate)==1 else sspTemplate
+        self.nTemplate   = dot(PoincareSection.zRotation(pi/2), self.sspTemplate) if len(nTemplate)==1 else nTemplate
+        self.integrator  = integrator
         self.dynamics    = dynamics
 
     def U(self, ssp):
@@ -136,33 +147,46 @@ class PoincareSection:
         Plane equation for the Poincare section hyperplane which includes z-axis
         and makes an angle theta with the x-axis see ChaosBook ver. 14, fig. 3.2
         Inputs:
-        ssp: State space point at which the Poincare hyperplane equation will be
-             evaluated
+          ssp: State space point at which the Poincare hyperplane equation will be
+               evaluated
         Outputs:
-        U: Hyperplane equation which should be satisfied on the Poincare section
+          U: Hyperplane equation which should be satisfied on the Poincare section
            U = (ssp - sspTemplate) . nTemplate (see ChaosBook ver. 14, eq. 3.6)
         '''
         return dot((ssp - self.sspTemplate),self.nTemplate)
 
-    def interpolate(self,ts, orbit,i):
-        y = orbit[:,i]
-        dt0 = 0.5*(ts[i+1]-ts[i])
-        def Flow(y0,dt):
-            tt,yy=self.integrator.integrate(y0,dt)
-            return yy[0]
-        fdeltat = lambda t: self.U(Flow(y, dt0))
-        deltat              = fsolve(fdeltat, dt0)[0]
-        bunch = solve_ivp(dynamics.velocity, (0, deltat), y)
-        return bunch.t[1],bunch.y[:,1]
+    def Flow(self,y0,dt):
+        _,y = self.integrator.integrate(y0,dt)
+        return y[0]
+
+    def interpolate(self,dt0, y0):
+        return self.integrator.Flow(fsolve(lambda t: self.U(self.Flow(y0, t)), dt0)[0],
+                                    y0)
+
 
     def interections(self,ts, orbit):
         _,n = orbit.shape
         for i in range(n-1):
             if self.U(orbit[:,i])<0 and self.U(orbit[:,i+1])>0:
-                yield self.interpolate(ts,orbit,i)
+                yield self.interpolate(0.5*(ts[i+1]-ts[i]), orbit[:,i])
+
+def plot_poincare(ax,section,ts,orbit,s=1):
+    for t,point in section.interections(ts,orbit):
+        ax.scatter(point[0],point[1],point[2],
+                   c      = 'xkcd:green',
+                   s      = s,
+                   marker = 'o')
+
+    ax.scatter(point[0],point[1],point[2],
+               c      = 'xkcd:green',
+               s      = s,
+               label  = r'Poincar\'e return',
+               marker = 'o')
+
 
 if __name__ == '__main__':
-    parser        = ArgumentParser()
+    rcParams['text.usetex'] = True
+    parser                  = ArgumentParser(description = __doc__)
     parser.add_argument('action', type = int)
     parser.add_argument('--figs', default = './figs')
     args          = parser.parse_args()
@@ -172,46 +196,54 @@ if __name__ == '__main__':
         dynamics      = Lorentz()
         integrator    = Integrator(dynamics)
         EQs           = dynamics.create_eqs()
+        section       = PoincareSection(dynamics,integrator,
+                                        sspTemplate = EQs[2],
+                                        nTemplate   = array([1,0,0]))
+
         x0            = EQs[0,:] + 0.001*rand(3)
         dt            = 0.005
         nstp          = 50.0/dt
         ts,orbit      = integrator.integrate(x0, 50.0, nstp)
-        section       = PoincareSection(dynamics)
+
         ax  = fig.add_subplot(111, projection='3d')
         ax.plot(orbit[0,:], orbit[1,:], orbit[2,:],
+                c          = 'xkcd:blue',
+                label      = 'Orbit',
                 markersize = 1)
         ax.scatter(EQs[0,0], EQs[0,1], EQs[0,2], marker='o', c='xkcd:red', label='EQ0')
         ax.scatter(EQs[1,0], EQs[1,1], EQs[1,2], marker='1', c='xkcd:red', label='EQ1')
-        ax.scatter(EQs[2,0], EQs[2,1], EQs[2,2], marker='2',c='xkcd:red', label='EQ2')
-        for t,point in section.interections(ts,orbit):
-            ax.scatter(point[0],point[1],point[2],c='xkcd:green')
+        ax.scatter(EQs[2,0], EQs[2,1], EQs[2,2], marker='2', c='xkcd:red', label='EQ2')
+        plot_poincare(ax,section,ts,orbit)
         ax.set_title(dynamics.get_title())
         ax.set_xlabel(dynamics.get_x_label())
         ax.set_ylabel(dynamics.get_y_label())
         ax.set_zlabel(dynamics.get_z_label())
-        ax.legend()
 
     if args.action==2:
         dynamics   = PseudoLorentz()
         integrator = Integrator(dynamics)
         EQs        = dynamics.create_eqs()
+        section    = PoincareSection(dynamics,integrator,
+                                        sspTemplate = EQs[1],
+                                        nTemplate   = array([1,0,0]))
         x0         = EQs[0,:] + 0.001*rand(3)
         dt         = 0.001
         nstp       = 50.0/dt
-        ts,orbit = integrator.integrate(x0, 50.0, nstp)
+        ts,orbit   = integrator.integrate(x0, 50.0, nstp)
 
         ax  = fig.add_subplot(111, projection='3d')
         ax.plot(orbit[0,:], orbit[1,:], orbit[2,:],
-                markersize = 1)
+                markersize = 1,
+                c          = 'xkcd:blue',
+                label      = 'Orbit')
         ax.scatter(EQs[0,0], EQs[0,1], EQs[0,2], marker='o', c='xkcd:red', label='EQ0')
         ax.scatter(EQs[1,0], EQs[1,1], EQs[1,2], marker='1', c='xkcd:red', label='EQ1')
-
+        plot_poincare(ax,section,ts,orbit)
         ax.set_title(dynamics.get_title())
         ax.set_xlabel(dynamics.get_x_label())
         ax.set_ylabel(dynamics.get_y_label())
         ax.set_zlabel(dynamics.get_z_label())
 
-        ax.legend()
-
+    ax.legend()
     savefig(join(args.figs,f'{Path(__file__).stem}{args.action}'))
     show()
