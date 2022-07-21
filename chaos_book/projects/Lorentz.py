9af46deb8c7ce25b578248457ba32d48d81f508e
Simon Crase <simon@greenweaves.nz>
2022-07-22 11:05:37 +1200

    Plot interpolated arclengths

--- chaos_book/projects/Lorentz.py
+++ chaos_book/projects/Lorentz.py
@@ -1,11 +1,36 @@
-'''Investigation of Lorentz Equations'''
+#!/usr/bin/env python
+
+# Copyright (C) 2022 Simon Crase
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+# This program is intended to support my project from https://chaosbook.org/
+#  Chaos: Classical and Quantum - open projects
+
+'''Periodic orbits and desymmetrization of the Lorenz flow'''
+
+# Much of the code has been shamelessly stolen from Newton.py
+# on page https://phys7224.herokuapp.com/grader/homework3
+
 from argparse               import ArgumentParser
 from matplotlib.pyplot      import figure, rcParams, savefig, show, suptitle
-from numpy                  import arange, argmin, argsort, argwhere, array, cos, cross, dot, pi, sin, size, sqrt, zeros
+from numpy                  import arange, argmin, argsort, argwhere, array, cos, cross, dot, linspace, pi, sin, size, sqrt, zeros
 from numpy.random           import rand
 from os.path                import join, basename, split
 from pathlib                import Path
 from scipy.integrate        import solve_ivp
+from scipy.interpolate      import splev, splprep, splrep
 from scipy.linalg           import eig, norm
 from scipy.optimize         import fsolve
 from scipy.spatial.distance import pdist, squareform
@@ -180,6 +205,65 @@ class PoincareSection:
         y                 = dot(projected,self.e_2)
         return x,y
 
+    def create_arclengths(self):
+        ps                    = array([self.project(point) for _,point in self.interections(ts,orbit)])
+        Distance              = squareform(pdist(ps))
+        SortedPoincareSection = ps.copy()
+        ArcLengths            = zeros(size(SortedPoincareSection, 0))
+        sn                    = zeros(size(ps, 0)) # the arclengths of the Poincare section points keeping their dynamical order for use in the return map
+        for k in range(size(SortedPoincareSection, 0) - 1):
+            m             = argmin(Distance[k, k + 1:]) + k + 1     #Find the element which is closest to the kth point:
+            dummyPoincare = SortedPoincareSection[k + 1, :].copy()  #Hold the (k+1)th row in the dummy vector:
+            SortedPoincareSection[k + 1, :] = SortedPoincareSection[m, :]  #Replace (k+1)th row with the closest point:
+            SortedPoincareSection[m, :] = dummyPoincare    #Assign the previous (k+1)th row to the mth row:
+
+            #Rearrange the distance matrix according to the new form of the
+            #SortedPoincareSection array:
+            dummyColumn = Distance[:, k + 1].copy()  # Hold (k+1)th column of the
+                                                     # distance matrix in a dummy
+                                                     # array
+            Distance[:, k + 1] = Distance[:, m]  # Assign mth column to kth
+            Distance[:, m] = dummyColumn
+
+            dummyRow = Distance[k + 1, :].copy()  # Hold (k+1)th row in a dummy array
+            Distance[k + 1, :] = Distance[m, :]
+            Distance[m, :] = dummyRow
+
+            ArcLengths[k + 1] = ArcLengths[k] + Distance[k, k + 1] #Assign the arclength of (k+1)th element:
+            #Find this point in the PoincareSection array and assign sn to its
+            #corresponding arclength:
+            sn[argwhere(ps[:, 0] == SortedPoincareSection[k + 1, 0])] = ArcLengths[k + 1]
+
+        #Parametric spline interpolation to the Poincare section:
+        self.tckPoincare, u = splprep([SortedPoincareSection[:, 0], SortedPoincareSection[:, 1]],
+                                      u = ArcLengths,
+                                      s = 0)
+        sArray = linspace(min(ArcLengths), max(ArcLengths), 1000)
+        #Evaluate the interpolation:
+        InterpolatedPoincareSection = self.fPoincare(sArray)
+
+        sn1       = sn[0:-1]
+        sn2       = sn[1:]
+        isort     = argsort(sn1)
+        sn1       = sn1[isort]  # sort radii1
+        sn2       = sn2[isort]  # sort radii2
+        tckReturn = splrep(sn1,sn2)
+        snPlus1   = splev(sArray, tckReturn)
+        return sn1,sn2,sArray,snPlus1
+
+    def fPoincare(self,s):
+        '''
+        Parametric interpolation to the Poincare section
+        Inputs:
+        s: Arc length which parametrizes the curve, a float or dx1-dim numpy
+           array
+        Outputs:
+        xy = x and y coordinates on the Poincare section, 2-dim numpy array
+           or (dx2)-dim numpy array
+        '''
+        interpolation = splev(s, self.tckPoincare)
+        return array([interpolation[0], interpolation[1]], float).transpose()
+
 def plot_poincare(ax,section,ts,orbit,s=1):
     for t,point in section.interections(ts,orbit):
         ax.scatter(point[0],point[1],point[2],
@@ -218,7 +302,7 @@ if __name__ == '__main__':
         nstp          = 50.0/dt
         ts,orbit      = integrator.integrate(x0, 50.0, nstp)
 
-        ax  = fig.add_subplot(121, projection='3d')
+        ax  = fig.add_subplot(111, projection='3d')
         ax.plot(orbit[0,:], orbit[1,:], orbit[2,:],
                 c          = 'xkcd:blue',
                 label      = 'Orbit',
@@ -231,58 +315,8 @@ if __name__ == '__main__':
         ax.set_xlabel(dynamics.get_x_label())
         ax.set_ylabel(dynamics.get_y_label())
         ax.set_zlabel(dynamics.get_z_label())
-        ax.legend()
-        ax = fig.add_subplot(122)
-        ax.set_aspect('equal')
-        ps = array([section.project(point) for _,point in section.interections(ts,orbit)])
 
-        Distance = squareform(pdist(ps))
-        SortedPoincareSection = ps.copy()  # Copy PoincareSection into
-                                                        # a new variable
-        #Create a zero-array to assign arclengths of the Poincare section points
-        #after sorting:
-        ArcLengths = zeros(size(SortedPoincareSection, 0))
-        # Create another zero-array to assign the arclengths of the Poincare section
-        # points keeping their dynamical order for use in the return map
-        sn = zeros(size(ps, 0))
-        for k in range(size(SortedPoincareSection, 0) - 1):
-            #Find the element which is closest to the kth point:
-            m = argmin(Distance[k, k + 1:]) + k + 1
-            #Hold the (k+1)th row in the dummy vector:
-            dummyPoincare = SortedPoincareSection[k + 1, :].copy()
-            #Replace (k+1)th row with the closest point:
-            SortedPoincareSection[k + 1, :] = SortedPoincareSection[m, :]
-            #Assign the previous (k+1)th row to the mth row:
-            SortedPoincareSection[m, :] = dummyPoincare
 
-            #Rearrange the distance matrix according to the new form of the
-            #SortedPoincareSection array:
-            dummyColumn = Distance[:, k + 1].copy()  # Hold (k+1)th column of the
-                                                     # distance matrix in a dummy
-                                                     # array
-            Distance[:, k + 1] = Distance[:, m]  # Assign mth column to kth
-            Distance[:, m] = dummyColumn
-
-            dummyRow = Distance[k + 1, :].copy()  # Hold (k+1)th row in a dummy
-                                                  # array
-            Distance[k + 1, :] = Distance[m, :]
-            Distance[m, :] = dummyRow
-
-            #Assign the arclength of (k+1)th element:
-            ArcLengths[k + 1] = ArcLengths[k] + Distance[k, k + 1]
-            #Find this point in the PoincareSection array and assign sn to its
-            #corresponding arclength:
-            sn[argwhere(ps[:, 0]
-                           == SortedPoincareSection[k + 1, 0])] = ArcLengths[k + 1]
-        sn1 = sn[0:-1]
-        sn2 = sn[1:]
-
-        isort = argsort(sn1)
-        sn1   = sn1[isort]  # sort radii1
-        sn2   = sn2[isort]  # sort radii2
-        ax.plot(sn1, sn2, '.r',
-                markersize = 5,
-                label='sn1:sn2')
     if args.action==2:
         dynamics   = PseudoLorentz()
         integrator = Integrator(dynamics)
@@ -308,6 +342,37 @@ if __name__ == '__main__':
         ax.set_ylabel(dynamics.get_y_label())
         ax.set_zlabel(dynamics.get_z_label())
 
-        ax.legend()
+
+    if args.action==3:
+        dynamics      = Lorentz()
+        integrator    = Integrator(dynamics)
+        EQs           = dynamics.create_eqs()
+        section       = PoincareSection(dynamics,integrator,
+                                        sspTemplate = EQs[args.fp],
+                                        nTemplate   = array([1,0,0]))
+
+        x0            = EQs[0,:] + 0.001*rand(3)
+        dt            = 0.005
+        nstp          = 50.0/dt
+        ts,orbit      = integrator.integrate(x0, 50.0, nstp)
+        sn1,sn2,sArray,snPlus1 = section.create_arclengths()
+
+        ax = fig.add_subplot(111)
+        # ax.set_aspect('equal')
+        ax.scatter(sn1, sn2,
+                   color  = 'xkcd:red',
+                   marker = 'x',
+                   s      = 64,
+                   label  = 'Sorted')
+        ax.scatter(sArray, snPlus1,
+                   color  = 'xkcd:blue',
+                   marker = 'o',
+                   s      = 1,
+                   label = 'Interpolated')
+        ax.plot(sArray, sArray,
+                color = 'xkcd:black',
+                linestyle = 'dotted',
+                label     = '$y=x$')
+    ax.legend()
     savefig(join(args.figs,f'{Path(__file__).stem}{args.action}'))
     show()
