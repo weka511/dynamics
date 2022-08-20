43c7591b8af9848743e0df1db906efcc866be43c
Simon Crase <simon@greenweaves.nz>
2022-08-20 12:05:21 +1200

    Created new class to represent an Equilibrium

--- chaos_book/projects/eqs.py
+++ chaos_book/projects/eqs.py
@@ -19,9 +19,33 @@
 
 from Lorentz           import Rossler
 from matplotlib.pyplot import figure, plot, show, tight_layout
-from numpy             import arange
+from numpy             import arange, exp, imag, isreal, pi, real
+from numpy.linalg      import eig
 from scipy.integrate   import solve_ivp
-from scipy.linalg      import eig
+
+class Equilibrium:
+    '''This class represents one equilibrium point'''
+    @classmethod
+    def create(self,dynamics):
+        '''Create set of equilibria for specified Dynamics'''
+        eqs = dynamics.find_equilibria()
+        m,_ = eqs.shape
+        return [Equilibrium(dynamics,eqs[i,:]) for i in range(m)]
+
+    def __init__(self,dynamics,eq):
+        self.dynamics = dynamics
+        self.eq       = eq.copy()
+
+    def describe(self):
+        print (f'{self.eq}')
+        w,v = eig(self.dynamics.StabilityMatrix(self.eq))
+        for i in range(len(w)):
+            if isreal(w[i]):
+                print (f'{real(w[i])} {exp( real(w[i]))}')
+            elif imag(w[i])>0:
+                T = 2*pi/imag(w[i])
+                Lambda = exp(real(w[i])*T)
+                print (f'Period={T}, Lambda={Lambda}')
 
 def get_orbit(eqs,
               dt       = 100.0,
@@ -30,14 +54,17 @@ def get_orbit(eqs,
               sign     = +1,
               origin   = 0):
 
-    return solve_ivp(dynamics.Velocity,  (0.0, dt), eqs[origin] + sign*epsilon*(eqs[origin] - eqs[1-origin]),
+    return solve_ivp(dynamics.Velocity,  (0.0, dt), eqs[origin].eq + sign*epsilon*(eqs[origin].eq - eqs[1-origin].eq),
                          method = 'RK45',
                          t_eval = arange(0.0, dt, dt/nstp)).y
 
 
 if __name__=='__main__':
     dynamics    = Rossler()
-    eqs         = dynamics.find_equilibria()
+    eqs         = Equilibrium.create(dynamics)
+    for eq in eqs:
+        eq.describe()
+
     orbit_plus  = get_orbit(eqs, dt=50)
     orbit_minus = get_orbit(eqs, sign = -1)
 
@@ -48,20 +75,20 @@ if __name__=='__main__':
             c          = 'xkcd:blue',
             markersize = 1,
             label      = 'Orbit')
-    s1 =ax.scatter(eqs[0,0], eqs[0,1], eqs[0,2],
+    s1 =ax.scatter(orbit_plus[0,0],orbit_plus[1,0],orbit_plus[2,0],
                c      = 'xkcd:green',
                s      = 25,
                marker = 'X',
-               label  = f'({eqs[0,0]:.4f},{eqs[0,1]:.4f},{eqs[0,2]:.4f})')
+               label  = f'({orbit_plus[0,0]:.4f},{orbit_plus[0,1]:.4f},{orbit_plus[0,2]:.4f})')
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
 
-    ax = fig.add_subplot(222, projection='3d')
+    ax  = fig.add_subplot(222, projection='3d')
     ax.plot(orbit_minus[0,:],orbit_minus[1,:],orbit_minus[2,:],
             c          = 'xkcd:red',
             markersize = 1)
-    ax.scatter(eqs[0,0], eqs[0,1], eqs[0,2],
+    ax.scatter(orbit_minus[0,0], orbit_minus[1,0], orbit_minus[2,0],
                c      = 'xkcd:green',
                s      = 25,
                marker = 'X')
@@ -69,22 +96,19 @@ if __name__=='__main__':
     ax.set_ylabel('y')
     ax.set_zlabel('z')
 
-    orbit_plus  = get_orbit(eqs,
-                            origin= 1)
-    orbit_minus = get_orbit(eqs,
-                            sign = -1,
-                            origin= 1)
+    orbit_plus  = get_orbit(eqs, origin= 1)
+    orbit_minus = get_orbit(eqs, sign = -1, origin= 1)
 
     ax = fig.add_subplot(223, projection='3d')
 
     ax.plot(orbit_plus[0,:],orbit_plus[1,:],orbit_plus[2,:],
             c          = 'xkcd:blue',
             markersize = 1)
-    s2=ax.scatter(eqs[1,0], eqs[1,1], eqs[1,2],
+    s2=ax.scatter(orbit_plus[0,0], orbit_plus[1,0], orbit_plus[2,0],
                c      = 'xkcd:black',
                s      = 25,
                marker = '+',
-               label  = f'({eqs[1,0]:.4f},{eqs[1,1]:.4f},{eqs[1,2]:.4f})')
+               label  = f'({orbit_plus[0,0]:.4f},{orbit_plus[1,0]:.4f},{orbit_plus[2,0]:.4f})')
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
@@ -93,13 +117,13 @@ if __name__=='__main__':
     ax.plot(orbit_minus[0,:],orbit_minus[1,:],orbit_minus[2,:],
             c          = 'xkcd:red',
             markersize = 1)
-    ax.scatter(eqs[1,0], eqs[1,1], eqs[1,2],
+    ax.scatter(orbit_minus[0,0], orbit_minus[1,0], orbit_minus[2,0],
                c      = 'xkcd:black',
                s      = 25,
                marker = '+')
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
-    fig.legend(handles=[s1,s2],title='Equilibria')
+    fig.legend(handles = [s1,s2], title   = 'Equilibria')
     tight_layout()
     show()
