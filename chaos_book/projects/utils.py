1d6d05862db105c3c869cc721435a57e2bcf4c4e
Simon Crase <simon@greenweaves.nz>
2022-08-25 09:58:51 +1200

    Added doco comments

--- chaos_book/projects/utils.py
+++ chaos_book/projects/utils.py
@@ -22,24 +22,48 @@ from os.path           import basename, join, splitext
 from matplotlib.pyplot import figure,  savefig
 
 class Figure(AbstractContextManager):
-    '''Context manager for plotting a figure'''
+    '''Context manager for plotting and saving a figure'''
     def __init__(self,
                  figs     = './',
                  name     = '',
                  dynamics = None,
-                 file     = __file__):
-        self.figs     = figs
-        self.name     = name
-        self.dynamics = dynamics
-        self.file     = file
+                 file     = __file__,
+                 width    = 12,
+                 height   = 12):
+        '''
+        Initialize Figure
+
+        Parameters:
+            figs      Identifies location where figure file will be stored
+            file      1st cmponent of figure file name: should be __file__ for top level source file
+            dynamics  Used to create 2nd component of figure file name
+            name      Used to create 3rd component of figure file name
+            width     Width of figure in inches
+            height    Height of figure in inches
+        '''
+        self.figs        = figs
+        self.source_file = file
+        self.dynamics    = dynamics
+        self.name        = name
+        self.fig         = figure(figsize=(width,height))
 
     def __enter__(self):
-        self.fig = figure(figsize=(12,12))
         return self.fig
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        '''Save figure on exit'''
         if exc_type==None and exc_val==None and exc_tb==None:
-            savefig(join(self.figs,f'{splitext(basename(self.file))[0]}-{self.dynamics.name}-{self.name}'))
+            savefig(self.get_path_name())
             return True
         else:
             return False
+
+    def get_path_name(self):
+        '''
+        Identifies path where figure will be stored, comprising:
+           1. The folder where figure is to be stored
+           2. Name of top level source file for program, passed in a a parameter
+           3. Name of Dynamics (e.g. "Lorentz")
+           4. An additional compoient of the name.
+        '''
+        return join(self.figs,f'{splitext(basename(self.source_file))[0]}-{self.dynamics.name}-{self.name}')
