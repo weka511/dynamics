a856deb301b35b2bd23fd99944e7d8072256ca97
Simon Crase <simon@greenweaves.nz>
2022-09-03 14:54:27 +1200

    Used plot styles to distinquish plots

--- chaos_book/projects/integrators.py
+++ chaos_book/projects/integrators.py
@@ -25,6 +25,16 @@ from matplotlib.pyplot      import show
 from numpy                  import real
 from utils                  import Figure
 
+linestyles = [ # Used to distinguish individual plots
+    (0,(3,5)),
+    (0,(3,6)),
+    (0,(2,7)),
+    (0,(2,8)),
+    (0,(1,9)),
+    (0,(1,10)),
+]
+
+
 def parse_args():
     '''Parse command line arguments'''
     parser = ArgumentParser(description=__doc__)
@@ -60,13 +70,17 @@ if __name__=='__main__':
                 height   = 12) as fig:
         fig.suptitle(dynamics.get_title())
         ax   = fig.add_subplot(1,1,1,projection='3d')
+
+
         for i,method in enumerate(['RK45','RK23','DOP853','Radau','BDF','LSODA']):
             orbit       = Orbit(dynamics,
                                 dt          = args.dt,
                                 origin      = fp,
                                 direction   = real(v),
                                 eigenvalue  = w)
+
             ax.plot(orbit.orbit[0,:],orbit.orbit[1,:],orbit.orbit[2,:],
+                    linestyle = linestyles[i],
                 label = f'{method}')
 
         ax.set_xlabel('X')
