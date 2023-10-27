a67822dc8fe67b96e554ff459f8446982e072542
Simon Crase <simon@greenweaves.nz>
2023-10-27 14:55:14 +1300

    Close #19

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 Name | Description
 -------------------------- | ------------------------------------------------
+template.py|Template for python code
 chaos_book| Code written to accompany  [Chaos Book](http://chaosbook.org/) by Predrag Cvitanović et al
 strogatz|[Non Linear Dynamics and Chaos with Applications, 2nd Edition](http://www.stevenstrogatz.com/books/nonlinear-dynamics-and-chaos-with-applications-to-physics-biology-chemistry-and-engineering) by Steve Strogatz.
 simulation| Exercises from [Simulation and modeling of natural processes](https://www.coursera.org/learn/modeling-simulation-natural-processes/home/info)
---
+++ template.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+#   Copyright (C) 2023 Simon Crase
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+'''Template for python script for dynamics'''
+
+
+from argparse import ArgumentParser
+from os.path import join
+from time import time
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+
+def parse_args():
+    parser = ArgumentParser(description=__doc__)
+    return parser.parse_args()
+
+if __name__=='__main__':
+    start  = time()
+    args = parse_args()
+
+    elapsed = time() - start
+    minutes = int(elapsed/60)
+    seconds = elapsed - 60*minutes
+    print (f'Elapsed Time {minutes} m {seconds:.2f} s')
