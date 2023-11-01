98979ff637da54c9fe9ce2b3ff4656156d63087d
Simon Crase <simon@greenweaves.nz>
2023-11-01 15:55:49 +1300

    #1 Preliminary refactoring

--- strogatz/exercise_6_1.py
+++ strogatz/exercise_6_1.py
@@ -34,65 +34,11 @@ def get_name_for_save(extra=None,sep='-'):
 
 fig = figure()
 ax = fig.add_subplot(1,1,1)
-X,Y,U,V,fixed = generate(f = lambda x,y:(x-y,1-np.exp(x)))
-plot_phase_portrait(X,Y,U,V,fixed,title = r'$\dot{x} = x-y,\dot{y} = 1-e^x$',ax=ax)
-fig.suptitle('Example 6.1.1')
-fig.savefig(get_name_for_save(extra=1))
-
-fig = figure()
-ax = fig.add_subplot(1,1,1)
-X,Y,U,V,fixed = generate(f = lambda x,y:(x-x**3,-y))
-plot_phase_portrait(X,Y,U,V,fixed,title = r'$\dot{x} = x-x^3,\dot{y} = -y$',ax=ax)
-fig.suptitle('Example 6.1.2')
-fig.savefig(get_name_for_save(extra=2))
-
-fig = figure()
-ax = fig.add_subplot(1,1,1)
-X,Y,U,V,_ = generate(f = lambda x,y:(x*(x-y),y*(2*x-y)))
-plot_phase_portrait(X,Y,U,V,[(0,0)],title = r'$\dot{x} = x(x-y),\dot{y} = y*(2x-y)$',ax=ax)
-fig.suptitle('Example 6.1.3')
-fig.savefig(get_name_for_save(extra=3))
-
-fig = figure()
-ax = fig.add_subplot(1,1,1)
-X,Y,U,V,fixed = generate(f = lambda x,y:(y,x*(1+y)-1))
-plot_phase_portrait(X,Y,U,V,fixed,title = r'$\dot{x} = y,\dot{y} = x(1+y)-1$',ax=ax)
-fig.suptitle('Example 6.1.4')
-fig.savefig(get_name_for_save(extra=4))
-
-fig = figure()
-ax = fig.add_subplot(1,1,1)
-X,Y,U,V,fixed = generate(f = lambda x,y:(x*(2-x-y),x-y))
-plot_phase_portrait(X,Y,U,V,fixed,title = r'$\dot{x} = x(2-x-y),\dot{y} = x-y$',ax=ax)
-fig.suptitle('Example 6.1.5')
-fig.savefig(get_name_for_save(extra=5))
-
-fig = figure()
-ax = fig.add_subplot(1,1,1)
 X,Y,U,V,fixed = generate(f = lambda x,y:(x**2-y,x-y))
 plot_phase_portrait(X,Y,U,V,fixed,title = r'$\dot{x} = x^2-y,\dot{y} = x-y$',ax=ax)
 fig.suptitle('Example 6.1.6')
-fig.savefig(get_name_for_save(extra=6))
-
-fig = figure()
-ax = fig.add_subplot(1,1,1)
-X,Y,U,V,fixed = generate(f = lambda x,y:(-x-np.exp(-y),y),nx = 256, ny  =  256,xmin = -11,xmax = 10,ymin = 0,ymax = 20)
-plot_phase_portrait(X,Y,U,V,fixed,title = '$\dot{x} = -x-e^{-y},\dot{y} = y$',ax=ax)
-fig.suptitle('Example 6.1.7 showing Stable manifold')
-
-eps = 1e-6
-xy0 = [-1,eps]
-xy = [xy0]
-for j in range(10000):
-    xy.append(rk4(0.01,xy[-1],adapt(f = lambda x,y:(-x-np.exp(-y),y))))
-
-ax.plot([z[0] for z in xy],
-        [z[1] for z in xy],
-        c = 'r',
-        label = 'Stable manifold',
-        linewidth = 2)
-
-ax.legend(loc = 'best')
-fig.savefig(get_name_for_save(extra=7))
+fig.savefig(get_name_for_save())
 
 show()
+
+
