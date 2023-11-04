eec8d10301f38a217f976ba2a5870cc828b31419
Simon Crase <simon@greenweaves.nz>
2023-11-05 09:19:55 +1300

    #25: plot solutions to ODE

--- strogatz/exercise_3_1_2.py
+++ strogatz/exercise_3_1_2.py
@@ -23,6 +23,7 @@ from os.path import  basename,splitext
 from time import time
 import numpy as np
 from matplotlib.pyplot import figure, show
+from solver import rk4
 
 def parse_args():
     parser = ArgumentParser(description=__doc__)
@@ -33,22 +34,36 @@ def get_name_for_save(extra=None,sep='-'):
     basic = splitext(basename(__file__))[0]
     return basic if extra==None else f'{basic}{sep}{extra}'
 
+def plot_f(t,r,x,ax=None):
+    ax.plot(t,r - x)
+    ax.axhline(0,c='xkcd:red',linestyle=':')
+    ax.set_title(f'r={r}')
+
+def plot_solution(starts,r,N=1000,ax=None,a=0.45):
+    h = a/N
+    for start in starts:
+        y = np.zeros((N))
+        y[0] = start
+        for i in range(1,N):
+            y[i] = rk4(h,y[i-1],lambda y:r - np.cosh(y))
+        ax.plot (y)
+
 if __name__=='__main__':
     start  = time()
     args = parse_args()
-    fig = figure()
+    fig = figure(figsize=(20,20))
     ax = fig.add_subplot(2,2,1)
     t = np.linspace(-2,2, 100)
     x = np.cosh(t)
+    rng = np.random.default_rng()
+    starts = rng.choice(t,size=13)
     for i,r in enumerate([2,1,0]):
-        ax = fig.add_subplot(2,2,i+1)
-        ax.plot(t,r - x)
-        ax.axhline(0,c='xkcd:red',linestyle=':')
-        ax.set_title(f'r={r}')
-        ax.set_xlabel('x')
-    fig.suptitle(r'3.1.2 $r = \cosh{x}$')
+        plot_f(t,r,x,ax = fig.add_subplot(2,3,i+1))
+        plot_solution(starts,r,ax = fig.add_subplot(2,3,i+4))
+
+    fig.suptitle(r'3.1.2 $r - \cosh{x}$')
+
     fig.savefig(get_name_for_save())
-    fig.tight_layout()
     elapsed = time() - start
     minutes = int(elapsed/60)
     seconds = elapsed - 60*minutes
