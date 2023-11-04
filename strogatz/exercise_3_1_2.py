fdfaa146b4da7b23687b29c8ba77d4feaeca2d0b
Simon Crase <simon@greenweaves.nz>
2023-11-05 12:33:14 +1300

    #25: added bifurcation diagram

--- strogatz/exercise_3_1_2.py
+++ strogatz/exercise_3_1_2.py
@@ -43,26 +43,27 @@ def create_fixed(r):
     if r > 1:
         s = np.sqrt(r**2-1)
         return [np.log(r+s),np.log(r-s)]
-    elif r==1:
-        return [0]
-    else:
+    elif r < 1:
         return []
+    else:
+        return [0]
 
-def plot_solution(starts,r,N=1000,ax=None,a=0.5):
+def plot_solution(starts,r,N=1000,ax=None,a=0.5,L=10):
     h = a/N
     for start in starts:
-        y = np.zeros((N))
+        y = np.full((N),np.nan)
         y[0] = start
         for i in range(1,N):
-            y[i] = rk4(h,y[i-1],lambda y:r - np.cosh(y))
+            temp = rk4(h,y[i-1],lambda y:r - np.cosh(y))
+            if abs(temp>L):
+                break
+            else:
+                y[i] = temp
         ax.plot (y)
     for y0 in create_fixed(r):
         ax.axhline(y0,c='xkcd:red',linestyle='dashed')
 
-if __name__=='__main__':
-    start  = time()
-    args = parse_args()
-    fig = figure(figsize=(20,20))
+def plot_main(fig = None):
     ax = fig.add_subplot(2,2,1)
     t = np.linspace(-2,2, 100)
     x = np.cosh(t)
@@ -71,10 +72,40 @@ if __name__=='__main__':
     for i,r in enumerate([2,1,0]):
         plot_f(t,r,x,ax = fig.add_subplot(2,3,i+1))
         plot_solution(starts,r,ax = fig.add_subplot(2,3,i+4))
-
     fig.suptitle(r'3.1.2 $r - \cosh{x}$')
+    fig.savefig(get_name_for_save(extra=1))
 
-    fig.savefig(get_name_for_save())
+def plot_bifurcation(fig = None):
+    ax = fig.add_subplot(1,1,1)
+    r = np.concatenate((np.linspace(0.75,1,200), np.linspace(1,2,200))) # Force inclusion of r==1
+    x_stable = np.full((len(r)),np.nan)
+    x_unstable = np.full((len(r)),np.nan)
+    x_null = np.full((len(r)),np.nan)
+    for i in range(len(r)):
+        y = create_fixed(r[i])
+        if len(y) > 1:
+            x_stable[i] = y[0]
+            x_unstable[i] = y[1]
+        elif len(y) == 1:
+            x_stable[i] = y[0]
+            x_unstable[i] = y[0]
+        else:
+            x_null[i] = 0
+    ax.plot(r,x_stable,c='xkcd:blue',label='Stable')
+    ax.plot(r,x_unstable,linestyle='dashed',c='xkcd:red',label='Unstable')
+    ax.plot(r,x_null,linestyle='dotted',c='xkcd:red',label='Null')
+    ax.legend()
+    ax.set_xlabel('r')
+    ax.set_ylabel('x')
+    ax.set_title(r'3.1.2 Bifurcation diagram for $\dot{x} = r - \cosh{x}$')
+    fig.savefig(get_name_for_save(extra=2))
+
+if __name__=='__main__':
+    start  = time()
+    args = parse_args()
+    plot_main(fig=figure(figsize=(20,20)))
+    plot_bifurcation(fig=figure(figsize=(20,20)))
+
     elapsed = time() - start
     minutes = int(elapsed/60)
     seconds = elapsed - 60*minutes
