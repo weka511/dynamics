323d792ea0d6572f52253ac9ea10f7d4d7e1173f
Simon Crase <simon@greenweaves.nz>
2023-11-24 15:24:47 +1300

    #28 First plot of monte-carlo: not valiated

--- chaos_book/chapter9/trapped.py
+++ chaos_book/chapter9/trapped.py
@@ -24,14 +24,14 @@ from time import time
 import numpy as np
 from numpy.random import default_rng
 from matplotlib.pyplot import figure, show
-from pinball import Create_Centres, generate
+from pinball import Create_Centres, generate, create_pt, p_to_velocity
 
 def parse_args():
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('-R', '--R', type=float, default=6.0, help='Centre to Centre distance')
     parser.add_argument('-a', '--a', type=float, default=1.0, help='Radius of each Disk')
     parser.add_argument('--N', default=100000, type=int,help='Number of trajectories')
-    parser.add_argument('--seed', default=42, type=int,help='I')
+    parser.add_argument('--seed', default=42, type=int,help='Initialize random number generator')
     parser.add_argument('--show',  default=False, action='store_true', help='Show plots')
     return parser.parse_args()
 
@@ -45,13 +45,31 @@ def create_starting_values(N,rng = default_rng(),a=1):
     Starts = rng.random((args.N,2))
     return Starts*np.array([2*np.pi*a,2]) - np.array([0,1])
 
+def monte_carlo(N,rng = default_rng(),a=1,Centres=None):
+    for i in range(N):
+        s = 2 * np.pi * rng.random()
+        p = 2*rng.random() - 1
+        count = 0
+        for _,_,_,_,_,_ in generate(create_pt(s,radius=a,Centre=Centres[0]),
+                                    p_to_velocity(p), Centres, a = a):
+            count += 1
+        if count > 0:
+            yield s,p,count
+
 if __name__=='__main__':
     start  = time()
     args = parse_args()
-    Pos = create_starting_values(args.N,rng = default_rng(args.seed),a=args.a)
     fig = figure(figsize=(10,10))
     ax = fig.add_subplot(1,1,1)
-    ax.scatter(Pos[:,0],Pos[:,1],s=1)
+
+    ss = []
+    ps = []
+    counts = []
+    for s,p,count in monte_carlo(args.N,rng = default_rng(args.seed),a=args.a,Centres = Create_Centres(args.R)):
+        ss.append(s)
+        ps.append(p)
+        counts.append(count)
+    ax.scatter(ss,ps,s=1,c=counts)
     ax.set_xlim(0,2*np.pi*args.a)
     ax.set_ylim(-1,1)
     fig.savefig(get_name_for_save())
