9174f5f3355d46b865cde169444682c6e394a26e
Simon Crase <simon@greenweaves.nz>
2023-11-29 18:32:41 +1300

    #32 Created skeleton

--- strogatz/README.md
+++ strogatz/README.md
@@ -34,7 +34,8 @@
 -|exercise_6_4_4.py|Plot phase portraits for simplified Competitive Lotka–Volterra equations
 -|exercise_6_4_8.py|Plot phase portraits for evolutionary dynamics
 -|exercise_6_5_19.py|Plot phase portraits for Lotka–Volterra predator prey model
-8|example_8_7_1|Showlimit cycle of example 8.7.1
+8|example_8_2|Hopf Bifurcations
+-|example_8_7_1|Showlimit cycle of example 8.7.1
 -|exercises_8.tex|Theoretical Exercises from Strogatz
 10|exercise_10_6.py|Bifurcations
 
---
+++ strogatz/exercise_8_2.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+#   Copyright (C) 2023 Simon Crase
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+'''Template for python script for dynamics'''
+
+
+from argparse import ArgumentParser
+from os.path import  basename,splitext,join
+from time import time
+import numpy as np
+from matplotlib.pyplot import figure, show
+from phase import generate,plot_phase_portrait
+
+def parse_args():
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('--show',  default=False, action='store_true', help='Show plots')
+    return parser.parse_args()
+
+def get_name_for_save(extra = None,
+                      sep = '-',
+                      figs = './figs'):
+    '''
+    Extract name for saving figure
+
+    Parameters:
+        extra    Used if we want to save more than one figure to distinguish file names
+        sep      Used if we want to save more than one figure to separate extra from basic file name
+        figs     Path name for saving figure
+
+    Returns:
+        A file name composed of pathname for figures, plus the base name for
+        source file, with extra ditinguising information if required
+    '''
+    basic = splitext(basename(__file__))[0]
+    name = basic if extra==None else f'{basic}{sep}{extra}'
+    return join(figs,name)
+
+if __name__=='__main__':
+    start  = time()
+    args = parse_args()
+
+    mu = -0.1
+    X,Y,U,V,fixed = generate(f = lambda x,y:(y+mu*x,-x+mu*y-x**2*y),nx = 256, ny = 256)
+    fig = figure()
+    ax = fig.add_subplot(2,1,1)
+    plot_phase_portrait(X,Y,U,V,fixed,title = r'$\mu = -0.1$', ax = ax)
+    ax = fig.add_subplot(2,1,2)
+    mu = 0.1
+    X,Y,U,V,fixed = generate(f = lambda x,y:(y+mu*x,-x+mu*y-x**2*y),nx = 256, ny = 256)
+    plot_phase_portrait(X,Y,U,V,fixed,title = r'$\mu = 0.1$', ax = ax)
+    fig.suptitle( r'$\dot{x}=y+\mu x,\dot{y}=-x + \mu y -x^2 y$')
+    fig.tight_layout(h_pad=1.5)
+
+    elapsed = time() - start
+    minutes = int(elapsed/60)
+    seconds = elapsed - 60*minutes
+    print (f'Elapsed Time {minutes} m {seconds:.2f} s')
+    if args.show:
+        show()
