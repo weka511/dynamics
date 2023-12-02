6889510cd4db33d3747e1a54e6536ae048930d96
Simon Crase <simon@greenweaves.nz>
2023-12-03 11:24:41 +1300

    #34 Calculate Lyapunov

--- chaos_book/chapter6/henon.py
+++ chaos_book/chapter6/henon.py
@@ -31,6 +31,8 @@ def parse_args():
     parser.add_argument('--N0',default = 1000, type=int)
     parser.add_argument('--m',default = 11, type=int)
     parser.add_argument('--epsilon', default= 0.0001, type=float)
+    parser.add_argument('--a', default= 1.4, type=float)
+    parser.add_argument('--b', default= 0.3, type=float)
     return parser.parse_args()
 
 def get_name_for_save(extra = None,
@@ -55,27 +57,49 @@ def get_name_for_save(extra = None,
 def henon(x=0,a=1.4,b=0.3):
     return np.array([1 - a*x[0]**2 + x[1], b*x[0]])
 
-def plot_henon(a=1.4,b=0.3,N=100000,N0=0,ax=None):
+def get_trajectory(a = 1.4,
+                   b = 0.3,
+                   N = 100000,
+                   N0 = 0):
+    trajectory = np.zeros((args.m,N,2))
     for i in range(args.m):
-        trajectory=np.zeros((N,2))
-        trajectory[0,:] = args.epsilon * rng.random(2)
+        trajectory[i,0,:] = args.epsilon * rng.random(2)
+        k = 0
         for j in range(1,N0):
-            trajectory[0,:] = henon( trajectory[0,:],a=a,b=b)
-        for j in range(1,N):
-            trajectory[j,:] = henon( trajectory[j-1,:],a=a,b=b)
-        ax.scatter(trajectory[:,0],trajectory[:,1],s=1)
-        ax.scatter(trajectory[-1,0],trajectory[-1,1],marker='+',s=100)
-    ax.set_title(f'a={a},Burn in = {N0:,}')
+            trajectory[i,1,:] = henon( trajectory[i,k,:],a=a,b=b)
+            k = 1
+        for j in range(k+1,N):
+            trajectory[i,j,:] = henon( trajectory[i,j-1,:],a=a,b=b)
+
+    return trajectory
+
+def get_lyapunov(trajectory):
+    diffs = trajectory[1:,:,:] - trajectory[0,:,:]
+    normed_diffs = np.linalg.norm(diffs,axis=-1)
+    normed_diffs /= normed_diffs[0]
+    log_normed_diffs = np.log(normed_diffs)
+    return log_normed_diffs
 
 if __name__=='__main__':
     start  = time()
     rng = np.random.default_rng()
     args = parse_args()
+
+    trajectory = get_trajectory(a=args.a,b=args.b,N=args.N,N0=args.N0)
+    log_normed_diffs = get_lyapunov(trajectory)
+
     fig = figure(figsize=(12,12))
-    plot_henon(a=1.4,b=0.3,N=args.N,ax=fig.add_subplot(2,2,1))
-    plot_henon(a=1.4,b=0.3,N=args.N,N0=args.N0,ax=fig.add_subplot(2,2,3))
-    plot_henon(a=1.39945219,b=0.3,N=args.N,ax=fig.add_subplot(2,2,2))
-    plot_henon(a=1.39945219,b=0.3,N=args.N,N0=args.N0,ax=fig.add_subplot(2,2,4))
+    ax1 = fig.add_subplot(1,2,1)
+    for i in range(args.m):
+        ax1.scatter(trajectory[i,:,0],trajectory[i,:,1],s=1)
+        ax1.scatter(trajectory[i,-1,0],trajectory[i,-1,1],marker='+',s=100)
+
+    ax2 = fig.add_subplot(1,2,2)
+    m,n = log_normed_diffs.shape
+    for i in range(m):
+        ax2.scatter(list(range(n)),log_normed_diffs[i,:],s=1)
+
+    fig.suptitle(f'a={args.a}, b=args.b, Burn in = {args.N0:,}')
     fig.savefig(get_name_for_save())
     elapsed = time() - start
     minutes = int(elapsed/60)
