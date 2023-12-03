dd017fe1af011e90fa91c843421e16834f9f94bb
Simon Crase <simon@greenweaves.nz>
2023-12-03 17:04:45 +1300

    #34 Refactored

--- chaos_book/exercises/lyapunov.py
+++ chaos_book/exercises/lyapunov.py
@@ -15,25 +15,27 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-'''Exercise 6.3 How unstable is the Hénon attractor?'''
+'''Calculate Lyapunov expenent'''
+
 
 from argparse import ArgumentParser
 from os.path import  basename,splitext,join
 from time import time
 import numpy as np
 from matplotlib.pyplot import figure, show
-from lyapunov import get_lyapunov
 
 def parse_args():
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('--show',  default=False, action='store_true', help='Show plots')
-    parser.add_argument('--N', default = 100000, type=int)
-    parser.add_argument('--N0', default =0, type=int)
-    parser.add_argument('--m', default = 11, type=int)
-    parser.add_argument('--epsilon', default = 0.000001, type=float)
-    parser.add_argument('--a', default= 1.4, type=float)
-    parser.add_argument('--b', default= 0.3, type=float)
     return parser.parse_args()
+
+def get_lyapunov(trajectory):
+    '''Used to calculate Lyaponov exponnent'''
+    differences_from_reference = trajectory[1:,:,:] - trajectory[0,:,:]
+    normed_differences = np.linalg.norm(differences_from_reference,axis=-1)
+    normed_differences /= normed_differences[:,0]
+    log_normed_diffs = np.log(normed_differences)
+    return log_normed_diffs
 
 def get_name_for_save(extra = None,
                       sep = '-',
@@ -54,47 +56,10 @@ def get_name_for_save(extra = None,
     name = basic if extra==None else f'{basic}{sep}{extra}'
     return join(figs,name)
 
-def henon(x=0,a=1.4,b=0.3):
-    return np.array([1 - a*x[0]**2 + x[1], b*x[0]])
-
-def get_trajectory(a = 1.4,
-                   b = 0.3,
-                   N = 100000,
-                   N0 = 0,
-                   m = 2,
-                   rng = np.random.default_rng()):
-    trajectory = np.zeros((m,N,2))
-    for i in range(m):
-        trajectory[i,0,:] = args.epsilon * rng.random(2)
-        k = 0
-        for j in range(1,N0):
-            trajectory[i,1,:] = henon( trajectory[i,k,:],a=a,b=b)
-            k = 1
-        for j in range(k+1,N):
-            trajectory[i,j,:] = henon( trajectory[i,j-1,:],a=a,b=b)
-
-    return trajectory
-
 if __name__=='__main__':
     start  = time()
     args = parse_args()
 
-    trajectory = get_trajectory(a=args.a,b=args.b,N=args.N,N0=args.N0,m=args.m)
-    log_normed_diffs = get_lyapunov(trajectory)
-
-    fig = figure(figsize=(12,12))
-    ax1 = fig.add_subplot(1,2,1)
-    for i in range(args.m):
-        ax1.scatter(trajectory[i,:,0],trajectory[i,:,1],s=1)
-        ax1.scatter(trajectory[i,-1,0],trajectory[i,-1,1],marker='+',s=100)
-
-    ax2 = fig.add_subplot(1,2,2)
-    m,n = log_normed_diffs.shape
-    for i in range(m):
-        ax2.scatter(list(range(n)),log_normed_diffs[i,:],s=1)
-
-    fig.suptitle(f'a={args.a}, b=args.b, Burn in = {args.N0:,}')
-    fig.savefig(get_name_for_save())
     elapsed = time() - start
     minutes = int(elapsed/60)
     seconds = elapsed - 60*minutes
