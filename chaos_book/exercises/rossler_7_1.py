fd206418cee4e02acc0f070d93f212ba657cfb39
Simon Crase <simon@greenweaves.nz>
2023-12-08 09:33:25 +1300

    #35 Plot Rossler an Poincare section

--- chaos_book/exercises/rossler_7_1.py
+++ chaos_book/exercises/rossler_7_1.py
@@ -24,11 +24,17 @@ from time import time
 import numpy as np
 from matplotlib.pyplot import figure, show
 from rossler import Rossler
+from solver import rk4
 
 def parse_args():
     '''Define and parse command line arguments'''
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('--show',  default=False, action='store_true', help='Show plots')
+    parser.add_argument('--N', default=10000, type=int)
+    parser.add_argument('--a', default= 0.2, type=float)
+    parser.add_argument('--b', default= 0.2, type=float)
+    parser.add_argument('--c', default= 5.0, type=float)
+    parser.add_argument('--delta_t', default= 0.01, type=float)
     return parser.parse_args()
 
 def get_name_for_save(extra = None,
@@ -50,11 +56,47 @@ def get_name_for_save(extra = None,
     name = basic if extra==None else f'{basic}{sep}{extra}'
     return join(figs,name)
 
+class Template:
+    def __init__(self,x,n):
+        self.x = x
+        self.n = n
+
+    def get_orientation(self,x):
+        return np.dot(x-self.x,self.n)
+
+def zRotation(theta):
+    '''
+    Rotation matrix about z-axis
+    Input:
+    theta: Rotation angle (radians)
+    Output:
+    Rz: Rotation matrix about z-axis
+    '''
+    return np.array([[np.cos(theta), -np.sin(theta), 0],
+                  [np.sin(theta), np.cos(theta),  0],
+                  [0,          0,           1]],
+                 float)
+
 if __name__=='__main__':
     start  = time()
     args = parse_args()
+
+    rossler = Rossler(a = args.a, b = args.b, c = args.c)
+    Orbit = np.empty((args.N,rossler.d))
+    Orbit[0,:] = np.zeros((3))
+    for i in range(1,args.N):
+        Orbit[i,:] = rk4(args.delta_t,Orbit[i-1],rossler.Velocity)
+
+    thetaPoincare = np.pi/4
+    e_x         = np.array([1, 0, 0], float)  # Unit vector in x-direction
+    sspTemplate = np.dot(zRotation(thetaPoincare), e_x)  #Template vector to define the Poincare section hyperplane
+    nTemplate   = np.dot(zRotation(np.pi/2), sspTemplate)  #Normal to this plane will be equal to template vector rotated pi/2 about the z axis
+
+    template = Template(sspTemplate,nTemplate)
+    orientation = template.get_orientation(Orbit)
     fig = figure(figsize=(12,12))
-    ax1 = fig.add_subplot(1,1,1)
+    ax1 = fig.add_subplot(1,1,1,projection='3d')
+    ax1.scatter(Orbit[:,0], Orbit[:,1], Orbit[:,2],c=orientation,s=1,cmap='seismic')
 
     elapsed = time() - start
     minutes = int(elapsed/60)
