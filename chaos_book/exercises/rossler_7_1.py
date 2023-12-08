e844e69437ae9e0b7228a5a95f88d3eb60db92df
Simon Crase <simon@greenweaves.nz>
2023-12-08 18:12:16 +1300

    #35 Rough calculation of zeros

--- chaos_book/exercises/rossler_7_1.py
+++ chaos_book/exercises/rossler_7_1.py
@@ -57,25 +57,49 @@ def get_name_for_save(extra = None,
     return join(figs,name)
 
 class Template:
-    def __init__(self,x,n):
-        self.x = x
-        self.n = n
+    '''Represents a flat Poincaré Section'''
+    @staticmethod
+    def create(thetaPoincare = np.pi/4):
+        e_x         = np.array([1, 0, 0], float)  # Unit vector in x-direction
+        sspTemplate = np.dot(Template.zRotation(thetaPoincare), e_x)  #Template vector to define the Poincare section hyperplane
+        nTemplate   = np.dot(Template.zRotation(np.pi/2), sspTemplate)  #Normal to this plane will be equal to template vector rotated pi/2 about the z axis
+        e_z          = np.array([0, 0, 1], float)  # Unit vector in z direction
+        ProjPoincare = np.array([sspTemplate,
+                                 e_z,
+                                 nTemplate])
+        return Template(sspTemplate,nTemplate,ProjPoincare)
+
+    @staticmethod
+    def zRotation(theta):
+        '''
+        Rotation matrix about z-axis
+        Input:
+        theta: Rotation angle (radians)
+        Output:
+        Rz: Rotation matrix about z-axis
+        '''
+        return np.array([[np.cos(theta), -np.sin(theta), 0],
+                         [np.sin(theta), np.cos(theta),  0],
+                         [0,             0,              1]])
+
+    def __init__(self,sspTemplate,nTemplate,ProjPoincare):
+        self.sspTemplate = sspTemplate
+        self.nTemplate = nTemplate
+        self.ProjPoincare = ProjPoincare
 
     def get_orientation(self,x):
-        return np.dot(x-self.x,self.n)
+        return np.dot(x-self.sspTemplate,self.nTemplate)
 
-def zRotation(theta):
-    '''
-    Rotation matrix about z-axis
-    Input:
-    theta: Rotation angle (radians)
-    Output:
-    Rz: Rotation matrix about z-axis
-    '''
-    return np.array([[np.cos(theta), -np.sin(theta), 0],
-                  [np.sin(theta), np.cos(theta),  0],
-                  [0,          0,           1]],
-                 float)
+    def get_projection(self,sspSolutionPoincare):
+        return np.dot(self.ProjPoincare, sspSolutionPoincare.transpose()).transpose()
+
+def get_intersections(orientation):
+    index_zero_crossings = np.where(np.diff(np.sign(orientation)))[0]
+    m, = index_zero_crossings.shape
+    intersections = np.empty((m,3))
+    for i in range(m):
+        intersections[i,:] = 0.5*(Orbit[index_zero_crossings[i],:] + Orbit[index_zero_crossings[i]+1,:])
+    return intersections
 
 if __name__=='__main__':
     start  = time()
@@ -87,20 +111,38 @@ if __name__=='__main__':
     for i in range(1,args.N):
         Orbit[i,:] = rk4(args.delta_t,Orbit[i-1],rossler.Velocity)
 
-    thetaPoincare = np.pi/4
-    e_x         = np.array([1, 0, 0], float)  # Unit vector in x-direction
-    sspTemplate = np.dot(zRotation(thetaPoincare), e_x)  #Template vector to define the Poincare section hyperplane
-    nTemplate   = np.dot(zRotation(np.pi/2), sspTemplate)  #Normal to this plane will be equal to template vector rotated pi/2 about the z axis
-
-    template = Template(sspTemplate,nTemplate)
+    template = Template.create()
     orientation = template.get_orientation(Orbit)
+
+    intersections = get_intersections(orientation)
+    projection = template.get_projection(intersections)
+
     fig = figure(figsize=(12,12))
-    ax1 = fig.add_subplot(1,1,1,projection='3d')
-    ax1.scatter(Orbit[:,0], Orbit[:,1], Orbit[:,2],c=orientation,s=1,cmap='seismic')
+    ax1 = fig.add_subplot(2,1,1,projection='3d')
+    sc = ax1.scatter(Orbit[:,0], Orbit[:,1], Orbit[:,2],
+                c = np.sign(orientation),
+                s = 1,
+                cmap = 'bwr')
+    labels = [r'$x\prime$',r'$y\prime$',r'$z\prime$']
+    for i in range(3):
+        ax1.plot([0,20*template.ProjPoincare[i,0]],
+                 [0,20*template.ProjPoincare[i,1]],
+                 [0,20*template.ProjPoincare[i,2]],
+                 label=labels[i])
+    ax1.set_xlabel('x')
+    ax1.set_ylabel('y')
+    ax1.set_zlabel('z')
+    fig.colorbar(sc,ax=ax1)
+    ax1.legend()
+    ax1.set_title('Rössler Orbit')
+
+    ax2 = fig.add_subplot(2,1,2)
+    ax2.scatter(projection[:,0],projection[:,1],marker='+')
 
     elapsed = time() - start
     minutes = int(elapsed/60)
     seconds = elapsed - 60*minutes
     print (f'Elapsed Time {minutes} m {seconds:.2f} s')
+    fig.savefig(get_name_for_save())
     if args.show:
         show()
