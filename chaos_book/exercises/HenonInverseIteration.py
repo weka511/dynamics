670c899d06263bab198f8e3a41b17b5ad04b336f
Simon Crase <simon@greenweaves.nz>
2024-01-03 15:56:03 +1300

    #49 Implement list of cycles

--- chaos_book/exercises/HenonInverseIteration.py
+++ chaos_book/exercises/HenonInverseIteration.py
@@ -24,8 +24,6 @@ from time import time
 import numpy as np
 from matplotlib.pyplot import figure, show
 
-a = 6
-
 def sign(s):
     converted = int(s)
     if converted==0: return -1
@@ -39,6 +37,7 @@ def parse_args():
     parser.add_argument('--N', type = int, default = 1000)
     parser.add_argument('--M', type = int, default = 100)
     parser.add_argument('--S', type = sign, nargs = '+')
+    parser.add_argument('action',choices=['explore','list'])
     return parser.parse_args()
 
 def get_name_for_save(extra = None,
@@ -66,22 +65,59 @@ def get_sign(S,i):
 def get_p(S):
     return ''.join(['0' if s==-1 else '1' for s in S ])
 
+def calculate_cycle(rng,M,N,S,a=6):
+    X = rng.uniform(0,0.25,2*M + N)
+    for i in range(M):
+        for j in range(1,X.size-1):
+            X[j] =   get_sign(S,j) * np.sqrt((1-X[j-1]-X[j+1])/a)
+
+    Cycle = X[M:M+len(S)]
+    return Cycle,X
+
 if __name__=='__main__':
     start  = time()
     args = parse_args()
     rng = np.random.default_rng()
-    X = rng.uniform(0,0.5,2*args.M + args.N)
-    for i in range(args.M):
-        for j in range(1,X.size-1):
-            X[j] =   get_sign(args.S,j) * np.sqrt((1-X[j-1]-X[j+1])/a)
+    match(args.action):
+        case 'explore':
+            Cycle,X = calculate_cycle(rng,args.M,args.N,args.S)
 
-    Cycle = X[args.M:args.M+len(args.S)]
-    fig = figure(figsize=(12,12))
-    ax1 = fig.add_subplot(1,1,1)
-    ax1.scatter(range(args.N),X[args.M:-args.M],s=1)
-    avge =  r'$\Sigma_i x_{p,i}$'
-    ax1.set_title(f'p={get_p(args.S)}, {avge}={Cycle.sum():.6f}')
-    fig.savefig(get_name_for_save())
+            fig = figure(figsize=(12,12))
+            ax1 = fig.add_subplot(1,1,1)
+            ax1.scatter(range(args.N),X[args.M:-args.M],s=1)
+            avge =  r'$\Sigma_i x_{p,i}$'
+            ax1.set_title(f'p={get_p(args.S)}, {avge}={Cycle.sum():.6f}')
+            fig.savefig(get_name_for_save())
+
+        case 'list':
+            for S in [
+                [-1],
+                [1],
+                [1,-1],
+                [1,-1,-1],
+                [1,1,-1],
+                [1,-1,-1,-1],
+                [1,1,-1,-1],
+                [1,1,1,-1],
+                [1,-1,-1,-1,-1],
+                [1,1,-1,-1,-1],
+                [1,-1,1,0,0],
+                [1,1,1,-1,-1],
+                [1,1,-1,1,-1],
+                [1,1,1,1,-1],
+                [1,-1,-1,-1,-1,-1],
+                [1,1,-1,-1,-1,-1],
+                [1,-1,1,1,1,1],
+                [1,1,1,-1,-1,-1],
+                [1,1,-1,1,-1,-1],
+                [1,-1,1,1,-1,-1],
+                [1,1,1,1,-1,-1],
+                [1,1,1,-1,1,-1],
+                [1,1,1,1,1,-1]
+            ]:
+                Cycle,_ = calculate_cycle(rng,args.M, args.N,S)
+                print (f'{get_p(S):8s}\t{Cycle.sum():9.06f}')
+
     elapsed = time() - start
     minutes = int(elapsed/60)
     seconds = elapsed - 60*minutes
