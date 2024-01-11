e5fdcfce82e36016a7fdf079867c51da6c3c76ff
Simon Crase <simon@greenweaves.nz>
2024-01-11 16:21:31 +1300

    #53 Implemented equation (14.4)

---
+++ chaos_book/exercises/prime.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+#   Copyright (C) 2024 Simon Crase
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+'''Exercise 14.2 Generating Prime Cycles'''
+
+
+from argparse import ArgumentParser
+from os.path import  basename,splitext,join
+from time import time
+import numpy as np
+from matplotlib.pyplot import figure, show
+
+def parse_args():
+    '''Define and parse command line arguments'''
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('--show',  default=False, action='store_true', help='Show plots')
+    return parser.parse_args()
+
+def get_w(s):
+    '''
+    Equation (14.4)
+    '''
+    n = s.size
+    w = np.empty((n))
+    w[0] = s[0]
+    for i in range(1,n):
+        w[i] = w[i-1] if s[i] == 0 else (1 - w[i-1])
+    return w
+
+def get_name_for_save(extra = None,
+                      sep = '-',
+                      figs = './figs'):
+    '''
+    Extract name for saving figure
+
+    Parameters:
+        extra    Used if we want to save more than one figure to distinguish file names
+        sep      Used if we want to save more than one figure to separate extra from basic file name
+        figs     Path name for saving figure
+
+    Returns:
+        A file name composed of pathname for figures, plus the base name for
+        source file, with extra distinguising information if required
+    '''
+    basic = splitext(basename(__file__))[0]
+    name = basic if extra==None else f'{basic}{sep}{extra}'
+    return join(figs,name)
+
+if __name__=='__main__':
+    start  = time()
+    args = parse_args()
+    w = get_w(np.array([1,0,0,1,1]))
+    fig = figure(figsize=(12,12))
+    ax1 = fig.add_subplot(1,1,1)
+
+    fig.savefig(get_name_for_save())
+    elapsed = time() - start
+    minutes = int(elapsed/60)
+    seconds = elapsed - 60*minutes
+    print (f'Elapsed Time {minutes} m {seconds:.2f} s')
+    if args.show:
+        show()
